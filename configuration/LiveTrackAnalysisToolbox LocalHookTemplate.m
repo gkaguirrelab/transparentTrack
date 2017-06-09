3f07d382b059fad6b80d80acad9f8e271d72a2d7
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2017-06-09 10:03:58 -0400

    Added TbTb configuration files

---
+++ configuration/LiveTrackAnalysisToolbox LocalHookTemplate.m
@@ -0,0 +1,20 @@
+function LiveTrackfMRIToolboxLocalHook
+% LiveTrackfMRIToolboxLocalHook
+%
+% For use with the ToolboxToolbox.  Copy this into your
+% ToolboxToolbox localToolboxHooks directory (by defalut,
+% ~/localToolboxHooks) and delete "Template" from the filename
+%
+% The thing that this does is add subfolders of the project to the path as
+% well as define Matlab preferences that specify input and output
+% directories.
+%
+% You will need to edit the project location and i/o directory locations
+% to match what is true on your computer.
+
+%% Say hello
+fprintf('* Running LiveTrackfMRIToolboxLocalHook...');
+
+%% Set preferences
+
+% Currently nothing to set
