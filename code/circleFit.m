4b50a7436c151b8a1840196e2ba3eada4581ea91
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2017-06-12 13:45:05 -0400

    added input parser to circleFit

--- code/circleFit.m
+++ code/circleFit.m
@@ -1,30 +1,47 @@
-function [pCenters, pRadii,pMetric, gCenters, gRadii,gMetric, pupilRange, glintRange] = circleFit(I,params,pupilRange,glintRange)
+function [pCenters, pRadii,pMetric, gCenters, gRadii,gMetric, pupilRange, glintRange] = circleFit(I,pupilCircleThresh,glintCircleThresh,pupilRange,glintRange,varargin)
 
 % this function is used for both glint and pupil circle fitting.
 
-%% set default thresholds for pupil and glint
-% when using the function to find the glint the default pupil threshold
-% will be used with no need to declare it, and viceversa.
 
-if ~isfield(params,'pupilCircleThresh')
-    params.pupilCircleThresh = 0.06;
-end
+%% parse input and define variables
+p = inputParser;
+% required input
+p.addRequired('I',isa(grayI,'uint8'));
+p.addRequired('pupilCircleThresh', @isnumeric)
+p.addRequired('glintCircleThresh', @isnumeric)
+p.addRequired('pupilRange',@isnumeric);
+p.addRequired('glintRange',@isnumeric);
 
-if ~isfield(params,'glintCircleThresh')
-    params.glintCircleThresh = 0.999;
-end
 
-if ~isfield(params,'glintOut')
-    params.glintOut = 0.1;
-end
+% optional inputs
+pupilOnlyDefault = false;
+glintOutDefault = 0.1;
+dilateGlintDefault = 6;
+sensitivityDefault = 0.99;
+rangeAdjustDefault = 0.05;
+p.addParameter('pupilOnly', pupilOnlyDefault, @islogic);
+p.addParameter('glintOut', glintOutDefault, @isnumeric);
+p.addParameter('dilateGlint', dilateGlintDefault, @isnumeric);
+p.addParameter('sensitivity', sensitivityDefault, @isnumeric);
+p.addParameter('rangeAdjust', rangeAdjustDefault, @isnumeric);
+
+%parse
+p.parse(I,pupilCircleThresh,glintCircleThresh,pupilRange,glintRange,varargin{:})
+
+% define optional variables
+pupilOnly = p.Results.pupilOnly;
+glintOut = p.Results.glintOut;
+dilateGlint = p.Results.dilateGlint;
+sensitivity = p.Results.sensitivity;
+rangeAdjust = p.Results.rangeAdjust;
 
 %% circle fit
 
 % create blurring filter
-filtSize = round([0.01*min(params.imageSize) 0.01*min(params.imageSize) 0.01*min(params.imageSize)]);
+filtSize = round([0.01*min(size(I)) 0.01*min(size(I)) 0.01*min(size(I))]);
 
 % structuring element to dialate the glint
-se = strel('disk',params.dilateGlint);
+se = strel('disk',dilateGlint);
 
 % Filter for pupil
 padP = padarray(I,[size(I,1)/2 size(I,2)/2], 128);
@@ -33,11 +50,11 @@ pI = imfilter(padP,h);
 pI = pI(size(I,1)/2+1:size(I,1)/2+size(I,1),size(I,2)/2+1:size(I,2)/2+size(I,2));
 % Binarize pupil
 binP = ones(size(pI));
-binP(pI<quantile(double(pI(:)),params.pupilCircleThresh)) = 0;
+binP(pI<quantile(double(pI(:)),pupilCircleThresh)) = 0;
 
 % Filter for glint
 gI = ones(size(I));
-gI(I<quantile(double(pI(:)),params.glintCircleThresh)) = 0;
+gI(I<quantile(double(pI(:)),glintCircleThresh)) = 0;
 padG = padarray(gI,[size(I,1)/2 size(I,2)/2], 0);
 h = fspecial('gaussian',[filtSize(1) filtSize(2)],filtSize(3));
 gI = imfilter(padG,h);
@@ -52,12 +69,12 @@ warning('off','images:imfindcircles:warnForLargeRadiusRange');
 
 % Find the pupil
 [pCenters, pRadii,pMetric] = imfindcircles(binP,pupilRange,'ObjectPolarity','dark',...
-    'Sensitivity',params.sensitivity);
+    'Sensitivity',sensitivity);
 
 % Find the glint
-if ~params.pupilOnly
+if ~pupilOnly
     [gCenters, gRadii,gMetric] = imfindcircles(dbinG,glintRange,'ObjectPolarity','bright',...
-        'Sensitivity',params.sensitivity);
+        'Sensitivity',sensitivity);
 else
     gCenters = [NaN NaN];
     gRadii = NaN;
@@ -68,21 +85,21 @@ end
 warning('on','images:imfindcircles:warnForLargeRadiusRange');
 
 % Remove glints outside the pupil
-if ~params.pupilOnly
+if ~pupilOnly
     if ~isempty(pCenters) && ~isempty(gCenters)
         dists           = sqrt( (gCenters(:,1) - pCenters(1,1)).^2 + (gCenters(:,2) - pCenters(1,2)).^2 );
-        gCenters(dists>(1 + params.glintOut)*(pRadii(1)),:) = [];
-        gRadii(dists>(1 + params.glintOut)*(pRadii(1))) = [];
+        gCenters(dists>(1 + glintOut)*(pRadii(1)),:) = [];
+        gRadii(dists>(1 + glintOut)*(pRadii(1))) = [];
     end
 end
 
 % adjust the pupil range (for quicker processing)
 if ~isempty(pCenters)
-    pupilRange(1)   = min(floor(pRadii(1)*(1-params.rangeAdjust)),params.pupilRange(2));
-    pupilRange(2)   = max(ceil(pRadii(1)*(1 + params.rangeAdjust)),params.pupilRange(1));
+    pupilRange(1)   = min(floor(pRadii(1)*(1-rangeAdjust)),pupilRange(2)); %%% CHECH THIS, it was params.pupilRange 
+    pupilRange(2)   = max(ceil(pRadii(1)*(1 + rangeAdjust)),pupilRange(1));
 else
-    pupilRange(1)   = max(ceil(pupilRange(1)*(1 - params.rangeAdjust)),params.pupilRange(1));
-    pupilRange(2)   = min(ceil(pupilRange(2)*(1 + params.rangeAdjust)),params.pupilRange(2));
+    pupilRange(1)   = max(ceil(pupilRange(1)*(1 - rangeAdjust)),pupilRange(1));
+    pupilRange(2)   = min(ceil(pupilRange(2)*(1 + rangeAdjust)),pupilRange(2));
 end
 
 end
