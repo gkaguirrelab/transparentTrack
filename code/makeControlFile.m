2c9477cd807f1efa39286543566fea043a7d35df
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2017-06-19 11:39:24 -0400

    edited output format to table, added header

--- code/makeControlFile.m
+++ code/makeControlFile.m
@@ -1,35 +1,55 @@
-function makeControlFile(controlFileName, framesToCut, blinkFrames )
+function makeControlFile(controlFileName, framesToCut, blinkFrames)
 
-% makeControlFile: produces a mat and csv control file
+% makeControlFile: produces mat and csv control file
 
-% the mat Control file is an array with the following structure
-% [frameNumber U R ForceEllipse_params]
-% where U,R are params for the cuts described in cutPupil.m 
-% if U and R are both NaN, that frame contains a blink.
+% the mat Control file is table with the following structure
+% [frameNumber isBlink U R ForceEllipse_params]
+% where
+%   isBlink is a flag for blink frames
+%   U,R are params for the cuts as described in cutPupil.m 
+%   Force ellipse: 5 parameters of the manually drawn ellipse on the frame.
+%   If the values are non NaN, they will override the subsequent fitting
+%   steps.
 % 
-% Force ellipse: 5 parameters of the manually drawn ellipse on the frame.
-% If the values are non NaN, they will override the subsequent fitting
-% steps.
+% Input params
+% ============
+%    controlFileName : path to the control file WITHOUT FILE EXTENSION
+%    framesToCut : array with guesses on how to cut frames, as output
+%        by guessPupilCuts.m
+%     blinkFrames: array with information on the blink frames as output
+%        by findBlinks.m
+% 
+% 
+% Usage example
+% =============
+% 
+%  controlFileName = fullfile(outputDir,'ControlFile');
+%  makeControlFile(controlFileName, framesToCut, blinkFrames)
 
-%% add NANs to the blinks array
-blinks = padarray(blinkFrames,[0 2], nan,'post');
+%% add 1 to the blinks array
+blinks = padarray(blinkFrames,[0 1], 1,'post');
+
+%% add empty fields for cuts to the blink array
+
+blinks = padarray(blinks,[0 2], nan,'post');
 
 %% merge blink array into control file
 
-cuts = [framesToCut(:,1) framesToCut(:,3) framesToCut(:,4)];
+cuts = [framesToCut(:,1) zeros(size(framesToCut,1),1) framesToCut(:,3) framesToCut(:,4)];
 
 controlMat = union (cuts, blinks,'rows');
 
-%% add a column to flag manual correction
+%% add columns for ellipse params
  controlMat = [controlMat nan(length(controlMat),5)];
 
-%% save out control file
 
-save([ controlFileName '.mat'], 'controlMat')
 %% make control file in a table
 
 controlTable = array2table(controlMat,...
-    'VariableNames',{'Frame' 'U' 'R' 'ForceEllipse_1' 'ForceEllipse_2' 'ForceEllipse_3' 'ForceEllipse_4' 'ForceEllipse_5'});
+    'VariableNames',{'Frame' 'isBlink' 'U' 'R' 'ForceEllipse_1' 'ForceEllipse_2' 'ForceEllipse_3' 'ForceEllipse_4' 'ForceEllipse_5'});
+%% save out control file as a table
+
+save([ controlFileName '.mat'], 'controlTable')
 
 %% save table as csv
 writetable(controlTable,[ controlFileName '.csv']);
