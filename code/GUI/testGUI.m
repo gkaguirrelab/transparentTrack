331e4bb7b4a36f05bd0d510aa9a34b8962aff750
Giulia Frazzetta <giuliaf@med.mail.upenn.edu>
2017-06-26 14:48:03 -0400

    Removed nested functions
    For clarity, any additional function needed in the GUI will now be
    saved as a separate function in the GUI folder of this repo. Only
    object definitions and callbacks will live in GUI.m
    
    - created separate loadDataInGUI.m
    - create separate refreshInstructionList.m

--- code/GUI/testGUI.m
+++ code/GUI/testGUI.m
@@ -54,76 +54,14 @@ function testGUI_OpeningFcn(hObject, eventdata, handles, varargin)
 
 
 %% parse vargin
+% if a dropbox dir and param struct are passed in as variables, then load
+% the corresponding data immediately
 if nargin == 5
-    sandboxDir = varargin{1};
+    dropboxDir = varargin{1};
     params = varargin{2};
-end
-%% files naming
-controlFileName = fullfile(sandboxDir,params.outputDir, params.projectSubfolder, ...
-        params.subjectName,params.sessionDate,params.eyeTrackingDir, ...
-        [params.runName '_controlFile.csv']);
-glintFileName = fullfile(sandboxDir,params.outputDir, params.projectSubfolder, ...
-        params.subjectName,params.sessionDate,params.eyeTrackingDir, ...
-        [params.runName '_glint.mat']);
-perimeterVideoName = fullfile(sandboxDir,params.outputDir, params.projectSubfolder, ...
-        params.subjectName,params.sessionDate,params.eyeTrackingDir, ...
-        [params.runName '_perimeter.avi']);
     
- inputVideo = fullfile(sandboxDir,params.outputDir, params.projectSubfolder, ...
-        params.subjectName,params.sessionDate,params.eyeTrackingDir, ...
-        [params.runName '_60hz.avi']);
-%% files loading
-disp('Loading Control File')
-% import control file
-handles.instructions = importControlFile(controlFileName);
-
-% import glint file (for cuts)
-disp('Loading Glint File')
-handles.glintFile = load(glintFileName);
-
-% load in the videos
-disp('Loading videos...')
-vid1 = VideoReader(inputVideo);
-vid2 = VideoReader(perimeterVideoName);
-handles.numFrames = 1000;%floor(vid2.Duration*vid2.FrameRate);
-% inizialize frame array
-handles.origVid = zeros([240 320 handles.numFrames],'uint8');
-handles.perimVid= zeros([240 320 handles.numFrames],'uint8');
-% get the videos in memory
-for ii = 1:handles.numFrames
-    thisFrame = readFrame(vid1);
-    tmp = rgb2gray(thisFrame);
-    tmp2 = imresize(tmp,[486 720]/2);
-    tmp = imcrop(tmp2,[1 1 319 239]);
-    handles.origVid(:,:,ii) = tmp;
+    handles = loadDataInGUI(dropboxDir, params, handles);
 end
-for ii = 1:handles.numFrames
-    thisFrame = readFrame(vid2);
-    tmp = rgb2gray(thisFrame);
-    handles.perimVid(:,:,ii) = tmp;
-end
-clear vid1
-clear vid2
-clear tmp
-
-%% first frame properties
-handles.frameNumber = 1;
-
-% set slider properties
-set(handles.slider1,'Value',1,'Min',1,'Max',handles.numFrames, 'SliderStep',[1/handles.numFrames, 10/handles.numFrames])
-
-% set frameNumberTxt Property
-set(handles.frameNumTxt, 'string', (num2str(handles.frameNumber)));
-
-% display first frame
-handles.frameNumber = 1;
-axes(handles.axes3);
-imshow(squeeze(handles.perimVid(:,:,handles.frameNumber)));
-axes(handles.axes4);
-imshow(squeeze(handles.origVid(:,:,handles.frameNumber)));
-
-% display instruction list
-handles = refreshInstructionList(handles);
 
 % Choose default command line output for testGUI
 handles.output = hObject;
@@ -509,14 +447,5 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
 end
 
 
-% I wrote this
-function handles = refreshInstructionList(handles)
-instructionLines = find ([handles.instructions.frame] == handles.frameNumber);
-    
-    if isempty (instructionLines) % check control file
-        ListOfImageNames = {};
-    else
-        ListOfImageNames = 1:1:length(instructionLines);
-    end
-    set(handles.instrunctionList,'string',ListOfImageNames);
+
 
