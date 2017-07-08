59651c96afafa00610640f7f812e8d6ceb54bbcd
gkaguirre <gkaguirre@me.com>
2017-07-08 13:01:18 -0400

    Small re-arrangement
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/processVideos/processVideoPipeline.m
+++ code/processVideos/processVideoPipeline.m
@@ -24,66 +24,63 @@ if ~exist(pathParams.controlFileDirFull,'dir')
 end
 
 
-%% Determine if the suffix of the raw file is "_raw.mov" or ".mov"
+%% Define inout and output filenames
+
+% Determine if the suffix of the raw file is "_raw.mov" or ".mov"
 if exist(fullfile(pathParams.dataSourceDirFull,[pathParams.runName '_raw.mov']),'file')
     rawVideoName = fullfile(pathParams.dataSourceDirFull,[pathParams.runName '_raw.mov']);
 else
     rawVideoName = fullfile(pathParams.dataSourceDirFull,[pathParams.runName '.mov']);
 end
 
+grayVideoName = fullfile(pathParams.dataOutputDirFull, [pathParams.runName '_gray.avi']);
+glintFileName = fullfile(pathParams.dataOutputDirFull, [pathParams.runName '_glint.mat']);
+perimeterFileName = fullfile(pathParams.dataOutputDirFull, [pathParams.runName '_perimeter.mat']);
+controlFileName = fullfile(pathParams.controlFileDirFull, [pathParams.runName '_controlFile.csv']);
+correctedPerimeterFileName = fullfile(pathParams.dataOutputDirFull, [pathParams.runName '_correctedPerimeter.mat']);
+ellipseFitFileName = fullfile(pathParams.dataOutputDirFull, [pathParams.runName '_pupil.mat']);
+finalFitVideoName = fullfile(pathParams.dataOutputDirFull, [pathParams.runName '_finalFit.mat']);
+
 
 %% Conduct the analysis
 
 % Convert raw video to cropped, resized, 60Hz gray
-grayVideoName = fullfile(pathParams.dataOutputDirFull, [pathParams.runName '_gray.avi']);
 raw2gray(rawVideoName,grayVideoName, varargin{:});
-
 if strcmp(p.Results.lastStage,'raw2gray')
     return
 end
 
 % track the glint
-glintFileName = fullfile(pathParams.dataOutputDirFull, [pathParams.runName '_glint.mat']);
 trackGlint(grayVideoName, glintFileName, varargin{:});
-
 if strcmp(p.Results.lastStage,'trackGlint')
     return
 end
 
 % extract pupil perimeter
-perimeterFileName = fullfile(pathParams.dataOutputDirFull, [pathParams.runName '_perimeter.mat']);
 extractPupilPerimeter(grayVideoName, perimeterFileName, varargin{:});
-
 if strcmp(p.Results.lastStage,'extractPupilPerimeter')
     return
 end
 
 % generate preliminary control file
-controlFileName = fullfile(pathParams.controlFileDirFull, [pathParams.runName '_controlFile.csv']);
 makePreliminaryControlFile(controlFileName, perimeterFileName, glintFileName, varargin{:});
-
 if strcmp(p.Results.lastStage,'makePreliminaryControlFile')
     return
 end
 
 % correct the perimeter video
-correctedPerimeterFileName = fullfile(pathParams.dataOutputDirFull, [pathParams.runName '_correctedPerimeter.mat']);
 correctPupilPerimeter(perimeterFileName,controlFileName,correctedPerimeterFileName, varargin{:});
-
 if strcmp(p.Results.lastStage,'correctPupilPerimeter')
     return
 end
 
 % bayesian fit of the pupil on the corrected perimeter video
-ellipseFitFileName = fullfile(pathParams.dataOutputDirFull, [pathParams.runName '_pupil.mat']);
 bayesFitPupilPerimeter(correctedPerimeterFileName, ellipseFitFileName, varargin{:});
-
 if strcmp(p.Results.lastStage,'bayesFitPupilPerimeter')
     return
 end
 
 % create a video of the final fit
-finalFitVideoName = fullfile(pathParams.dataOutputDirFull, [pathParams.runName '_finalFit.mat']);
 makePupilFitVideo(grayVideoName, finalFitVideoName, ...
     'glintFileName', glintFileName, 'perimeterFileName', correctedPerimeterFileName,...
     'ellipseFitFileName', ellipseFitFileName, 'whichFieldToPlot', 'pPosteriorMeanTransparent', ...
