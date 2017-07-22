2c986cd6e898b6f0a90774a01047df3cc45c4f1f
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2017-07-21 20:30:24 -0400

    Condensed the processPipeline in a compact for loop.
    Also fixed the count for the attempts in the try catch loop to start
    from 1 instead then 0.

--- code/processVideos/processVideoPipeline.m
+++ code/processVideos/processVideoPipeline.m
@@ -1,7 +1,7 @@
 function processVideoPipeline( pathParams, varargin )
-% 
-% this is the standard processing pipeline for eye tracking videos. 
-% 
+%
+% this is the standard processing pipeline for eye tracking videos.
+%
 % The pipeline consists in the following stages:
 %   raw2gray
 %   trackGlint
@@ -11,7 +11,7 @@ function processVideoPipeline( pathParams, varargin )
 %   bayesFitPupilPerimeter
 %   fitIrisAndPalpebralFissure
 %   makePupilFitVideo
-% 
+%
 % The user can stop the execution after any of the stages with the optional
 % param 'lastStage', or skip any amount of stages listing them in a cell
 % under the optional param 'skipStage'. Every stage however requires the
@@ -44,6 +44,7 @@ end
 
 %% Define input and output filenames
 
+
 % Determine if the suffix of the raw file is "_raw.mov" or ".mov"
 if exist(fullfile(pathParams.dataSourceDirFull,[pathParams.runName '_raw.mov']),'file')
     rawVideoName = fullfile(pathParams.dataSourceDirFull,[pathParams.runName '_raw.mov']);
@@ -64,24 +65,42 @@ irisFileName = fullfile(pathParams.dataOutputDirFull, [pathParams.runName '_iris
 % NOTE: some of the analysis steps are wrapped in a while+try/catch loop to
 % fix the matlabprefs.mat corruption bug.
 
-% Convert raw video to cropped, resized, 60Hz gray
-if ~any(strcmp(p.Results.skipStage,'raw2gray'))
-    raw2gray(rawVideoName,grayVideoName, varargin{:});
-    if strcmp(p.Results.lastStage,'raw2gray')
-        return
-    end
-end
+funCalls = {...
+    'raw2gray(rawVideoName,grayVideoName, varargin{:});'...
+    'trackGlint(grayVideoName, glintFileName, varargin{:});'...
+    'trackGlint(grayVideoName, glintFileName, varargin{:});'...
+    'extractPupilPerimeter(grayVideoName, perimeterFileName, varargin{:});'...
+    'correctPupilPerimeter(perimeterFileName,controlFileName,correctedPerimeterFileName, varargin{:});' ...
+    'bayesFitPupilPerimeter(correctedPerimeterFileName, pupilFileName, varargin{:});'...
+    'fitIrisCircleAndMask(grayVideoName, perimeterFileName, pupilFileName, irisFileName, varargin{:});' ...
+    ['makePupilFitVideo(grayVideoName, finalFitVideoName,' ...
+    '''glintFileName'', glintFileName, ''perimeterFileName'', correctedPerimeterFileName,'...
+    '''pupilFileName'', pupilFileName, ''whichFieldToPlot'', ''pPosteriorMeanTransparent'',' ...
+    '''irisFileName'', irisFileName,' ...
+    '''controlFileName'',controlFileName,varargin{:});']...
+    };
 
-% track the glint
-if ~any(strcmp(p.Results.skipStage,'trackGlint'))
-    % intialize while control
-    success = 0;
-    attempts = 0; % we will attempt to execute the instruction 3 times. If it does not work, the code will eventually break.
-    while ~success
-        try
-            trackGlint(grayVideoName, glintFileName, varargin{:});
-            success = 1;
-        catch ME
+funNames = { ...
+    'raw2gray'...
+    'trackGlint'...
+    'extractPupilPerimeter'...
+    'makePreliminaryControlFile'...
+    'correctPupilPerimeter'...
+    'bayesFitPupilPerimeter'...
+    'fitIrisAndPalpebralFissure'...
+    'makePupilFitVideo'...
+    };
+
+for ff = 1:length(funCalls)
+    if ~any(strcmp(p.Results.skipStage,funNames{ff}))
+        % intialize while control
+        success = 0;
+        attempts = 1; % we will attempt to execute the instruction 3 times. If it does not work, the code will eventually break.
+        while ~success
+            try
+                eval(funCalls{ff});
+                success = 1;
+            catch ME
                 warning ('There has been an error during execution. Cleaning matlabprefs.mat and trying again - attempt %d.',attempts)
                 matlabprefsCleanup;
                 success = 0;
@@ -89,178 +108,14 @@ if ~any(strcmp(p.Results.skipStage,'trackGlint'))
                 if attempts > 3
                     rethrow(ME)
                 end
+            end
+        end
+        % clear while and if control
+        clear success
+        clear attempts
+        if strcmp(p.Results.lastStage,funNames{ff})
+            return
         end
     end
-    % clear while and if control
-    clear success
-    clear attempts
-    if strcmp(p.Results.lastStage,'trackGlint')
-        return
-    end
 end
-
-% extract pupil perimeter
-if ~any(strcmp(p.Results.skipStage,'extractPupilPerimeter'))
-    % intialize while control
-    success = 0;
-    attempts = 0; % we will attempt to execute the instruction 3 times. If it does not work, the code will eventually break.
-    while ~success
-        try
-            extractPupilPerimeter(grayVideoName, perimeterFileName, varargin{:});
-            success = 1;
-        catch ME
-                warning ('There has been an error during execution. Cleaning matlabprefs.mat and trying again - attempt %d.',attempts)
-                matlabprefsCleanup;
-                success = 0;
-                attempts = attempts +1;
-                if attempts > 3
-                    rethrow(ME)
-                end
-        end
-    end
-    % clear while and if control
-    clear success
-    clear attempts
-    if strcmp(p.Results.lastStage,'extractPupilPerimeter')
-        return
-    end
 end
-
-% generate preliminary control file
-if ~any(strcmp(p.Results.skipStage,'makePreliminaryControlFile'))
-    % intialize while control
-    success = 0;
-    attempts = 0; % we will attempt to execute the instruction 3 times. If it does not work, the code will eventually break.
-    while ~success
-        try
-            makePreliminaryControlFile(controlFileName, perimeterFileName, glintFileName, varargin{:});
-            success = 1;
-        catch ME
-                warning ('There has been an error during execution. Cleaning matlabprefs.mat and trying again - attempt %d.',attempts)
-                matlabprefsCleanup;
-                success = 0;
-                attempts = attempts +1;
-                if attempts > 3
-                    rethrow(ME)
-                end
-        end
-    end
-    % clear while and if control
-    clear success
-    clear attempts
-    if strcmp(p.Results.lastStage,'makePreliminaryControlFile')
-        return
-    end
-end
-
-% correct the perimeter video
-if ~any(strcmp(p.Results.skipStage,'correctPupilPerimeter'))
-    % intialize while control
-    success = 0;
-    attempts = 0; % we will attempt to execute the instruction 3 times. If it does not work, the code will eventually break.
-    while ~success
-        try
-            correctPupilPerimeter(perimeterFileName,controlFileName,correctedPerimeterFileName, varargin{:});
-            success = 1;
-        catch ME
-                warning ('There has been an error during execution. Cleaning matlabprefs.mat and trying again - attempt %d.',attempts)
-                matlabprefsCleanup;
-                success = 0;
-                attempts = attempts +1;
-                if attempts > 3
-                    rethrow(ME)
-                end
-        end
-    end
-    % clear while and if control
-    clear success
-    clear attempts
-    if strcmp(p.Results.lastStage,'correctPupilPerimeter')
-        return
-    end
-end
-
-% bayesian fit of the pupil on the corrected perimeter video
-if ~any(strcmp(p.Results.skipStage,'bayesFitPupilPerimeter'))
-    % intialize while control
-    success = 0;
-    attempts = 0; % we will attempt to execute the instruction 3 times. If it does not work, the code will eventually break.
-    while ~success
-        try
-            bayesFitPupilPerimeter(correctedPerimeterFileName, pupilFileName, varargin{:});
-            success = 1;
-        catch ME
-                warning ('There has been an error during execution. Cleaning matlabprefs.mat and trying again - attempt %d.',attempts)
-                matlabprefsCleanup;
-                success = 0;
-                attempts = attempts +1;
-                if attempts > 3
-                    rethrow(ME)
-                end
-        end
-    end
-    % clear while and if control
-    clear success
-    clear attempts
-    if strcmp(p.Results.lastStage,'bayesFitPupilPerimeter')
-        return
-    end
-end
-
-% fit iris circle and mask
-if ~any(strcmp(p.Results.skipStage,'fitIrisCircleAndMask'))
-    % intialize while control
-    success = 0;
-    attempts = 0; % we will attempt to execute the instruction 3 times. If it does not work, the code will eventually break.
-    while ~success
-        try
-            fitIrisCircleAndMask(grayVideoName, perimeterFileName, pupilFileName, irisFileName, varargin{:});
-            success = 1;
-        catch ME
-                warning ('There has been an error during execution. Cleaning matlabprefs.mat and trying again - attempt %d.',attempts)
-                matlabprefsCleanup;
-                success = 0;
-                attempts = attempts +1;
-                if attempts > 3
-                    rethrow(ME)
-                end
-        end
-    end
-    % clear while and if control
-    clear success
-    clear attempts
-    if strcmp(p.Results.lastStage,'fitIrisAndPalpebralFissure')
-        return
-    end
-end
-
-% create a video of the final fit
-if ~any(strcmp(p.Results.skipStage,'makePupilFitVideo'))
-    % intialize while control
-    success = 0;
-    attempts = 0; % we will attempt to execute the instruction 3 times. If it does not work, the code will eventually break.
-    while ~success
-        try
-            makePupilFitVideo(grayVideoName, finalFitVideoName, ...
-        'glintFileName', glintFileName, 'perimeterFileName', correctedPerimeterFileName,...
-        'pupilFileName', pupilFileName, 'whichFieldToPlot', 'pPosteriorMeanTransparent', ...
-        'irisFileName', irisFileName, ...
-        'controlFileName',controlFileName,varargin{:});
-            success = 1;
-        catch ME
-                warning ('There has been an error during execution. Cleaning matlabprefs.mat and trying again - attempt %d.',attempts)
-                matlabprefsCleanup;
-                success = 0;
-                attempts = attempts +1;
-                if attempts > 3
-                    rethrow(ME)
-                end
-        end
-    end
-    % clear while and if control
-    clear success
-    clear attempts
-end
-
-end % function
-
