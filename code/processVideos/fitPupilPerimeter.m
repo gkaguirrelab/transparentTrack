6450e05a6666a6d622f9445c4d37ae5db9fb335b
gkaguirre <gkaguirre@me.com>
2017-08-14 15:08:52 -0400

    Removed warning off statements
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/processVideos/fitPupilPerimeter.m
+++ code/processVideos/fitPupilPerimeter.m
@@ -257,9 +257,6 @@ else
         fprintf('.\n');
     end
     
-    % temporarily suppress singular matrix warnings
-    warning('off','MATLAB:singularMatrix')
-    
     % Loop through the frames
     parfor (ii = 1:nFrames, nWorkers)
         
@@ -354,9 +351,6 @@ end % skipInitialPupilFit mode check
 if ~isempty(p.Results.pupilFileName)
     save(p.Results.pupilFileName,'pupilData')
 end
-
-% restore singular matrix warnings
-warning('on','MATLAB:singularMatrix')
     
 % If we are not skipping the Bayesian fitting, proceed
 if ~p.Results.skipPupilBayes
