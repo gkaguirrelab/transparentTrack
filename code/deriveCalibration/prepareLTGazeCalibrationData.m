aaf4f43c0ad8c5fb02fd51184ee5e15778d82e67
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2017-08-23 17:07:45 -0400

    Added alignment code for dots with no time information
    Note: code needs more work and testing as sometime returns wrong
    alignment. It is probably worth to add a “verify by eye” step to
    confirm alignment.

--- code/deriveCalibration/prepareLTGazeCalibrationData.m
+++ code/deriveCalibration/prepareLTGazeCalibrationData.m
@@ -6,13 +6,13 @@ function prepareLTGazeCalibrationData (LTdatFileName,gazeDataFileName,varargin)
 % LiveTrack device alone or with the Livetrack+VTop eye tracking system. It
 % will export a gazeData file containing all information needed to
 % calculate the gaze calibration conversion factors.
-% 
-% gazeCalData 
-%       targets.X = location on X axes with origin in the center of the 
+%
+% gazeCalData
+%       targets.X = location on X axes with origin in the center of the
 %           screen and growing left to right espressed as mm on screen
-%       targets.Y = location on Y axes with origin in the center of the 
+%       targets.Y = location on Y axes with origin in the center of the
 %           screen and growing top to bottom espressed as mm on screen
-%       pupil.X = raw location of pupil center in pixels on the frame (origin top left corner)  
+%       pupil.X = raw location of pupil center in pixels on the frame (origin top left corner)
 %       pupil.Y = raw location of pupil center in pixels on the frame (origin top left corner)
 %       glint.X = raw location of glint center in pixels on the frame (origin top left corner)
 %       glint.Y = raw location of glint center in pixels on the frame (origin top left corner)
@@ -21,7 +21,7 @@ function prepareLTGazeCalibrationData (LTdatFileName,gazeDataFileName,varargin)
 %           pupilRaw
 %           glintRaw
 %           fixDurationSec
-%           
+%
 
 
 %% input parser
@@ -36,6 +36,8 @@ p.addRequired('gazeDataFileName',@ischar);
 p.addParameter('useLiveTrackGazeData',false, @islogical)
 p.addParameter('ltFileSuffixLength', 10, @isnumeric)
 p.addParameter('frameRate', 60, @isnumeric)
+p.addParameter('medFilterOrder', 150, @isnumeric)
+p.addParameter('saccadeDistance', 30, @isnumeric)
 p.addParameter('movingMeanWindow', 1/3, @isnumerc)
 p.addParameter('removeBoundaryFrames', 1/10, @isnumeric)
 p.addParameter('viewingDistance', 1065, @isnumeric)
@@ -50,7 +52,7 @@ p.addParameter('username',char(java.lang.System.getProperty('user.name')),@ischa
 p.addParameter('hostname',char(java.net.InetAddress.getLocalHost.getHostName),@ischar);
 
 % parse
-p.parse(sizeDataFilesNames, sizeFactorsFileName, varargin{:})
+p.parse(LTdatFileName, gazeDataFileName, varargin{:})
 
 
 %% load LTdat file
@@ -72,9 +74,9 @@ gazeCalData.targets.Y     = LTdata.targets(:,2); % mm on screen, screen center =
 nanIDX = find(isnan(gazeCalData.targets.X));
 
 if ~isempty(nanIDX)
-   if length(nanIDX) > 1
-       error ('There is more than one NaN target! Calibration might fail.')
-   end
+    if length(nanIDX) > 1
+        error ('There is more than one NaN target! Calibration might fail.')
+    end
     % available targets locations
     highTRG = max(LTdata.targets(:,1));
     centerTRG = 0;
@@ -115,7 +117,7 @@ if p.Results.useLiveTrackGazeData
     % add metadata
     gazeCalData.meta = p.Results;
     
-    % save results 
+    % save results
     save (gazeDataFileName , 'gazeCalData')
     
     % no other further step is necessary
@@ -135,7 +137,7 @@ else
 end
 
 % get calibration file root name
-calFileRoot = ltDatFileName(1:end-p.Results.ltFileSuffixLength);
+calFileRoot = LTdatFileName(1:end-p.Results.ltFileSuffixLength);
 
 % get raw pupil and glint data
 rawPupilData = load ([calFileRoot '_pupil.mat']);
@@ -152,6 +154,11 @@ pupil.Y = rawPupilData.pupilData.pPosteriorMeanTransparent(:,2);
 % that the central target would be "seen" on the frame in the central
 % position [180 120] (if res [320, 240]), and all other targets would be
 % displaced 20 pixels around it according to their location.
+
+% available targets locations
+highTRG = max(LTdata.targets(:,1));
+centerTRG = 0;
+lowTRG = min(LTdata.targets(:,1));
 
 % conversion table (note that the X must be flipped because the high res
 % video is acquired mirrored).
@@ -172,13 +179,18 @@ targetPPX_Y(targetPPX_Y == lowTRG) = lowPPX(2);
 
 %% align target and pupil timeseries
 
-% extract pupil position and velocity
+% extract pupil position
 pupilPosition = sqrt((pupil.X).^2 + (pupil.Y).^2);
-pupilVelocity = diff(sqrt((pupil.X).^2 + (pupil.Y).^2));
 
-% remove nan from pupil position and velocity
+% remove nans
 pupilPosition = fillmissing(pupilPosition,'linear');
-pupilVelocity = fillmissing(pupilVelocity,'linear');
+
+% filter with high-order to remove spikes from noises and saccades
+pupilPosition = medfilt1(pupilPosition,p.Results.medFilterOrder,'truncate');
+
+% derive pupil velocity
+pupilVelocity = diff(pupilPosition);
+
 
 % if the dot times were recorded, we build the target timeseries in
 % pseudo-pixels and do a 2 steps cross correlation using position and
@@ -197,13 +209,17 @@ if dotTimesRecorded
     targetVelocity = diff(sqrt((target.X).^2 + (target.Y).^2));
     
     % first pass: cross correlate positions with a lag window length of
-    % half the target timeseries length. 
-    [r,lag]= xcorr(pupilPosition,targetPosition, round(length(targetPosition)/2));
+    % half the target timeseries length.
+    [r,lag]= xcorr(pupilPosition,targetPosition);%, round(length(targetPosition)/2));
     [~,I]= max(abs(r));
     delayP = lag(I);
     
     % trim the pupilPosition and the pupil velocity
-    pupilPositionTrim = pupilPosition(delayP:delayP+length(targetPosition));
+    if delayP == 0
+        pupilPositionTrim = pupilPosition;
+    else
+        pupilPositionTrim = pupilPosition(delayP:end);
+    end
     pupilVelocityTrim = diff(pupilPositionTrim);
     
     % second pass: cross correlate the velocity profile to refine the
@@ -212,52 +228,101 @@ if dotTimesRecorded
     [~,I]= max(abs(r));
     delayV = lag(I);
     
-    % refine pupil data
+    % refine pupil and glint data
     delay = delayP + delayV;
     pupil.X = pupil.X(delay:delay+length(target.X));
     pupil.Y = pupil.Y(delay:delay+length(target.Y));
+    glint.X = rawGlintData.glintData.X(delay:delay+length(target.X));
+    glint.Y = rawGlintData.glintData.Y(delay:delay+length(target.Y));
 end
 
 
-% if the dot times were not recorded...
+% if the dot times were not recorded we try to match the velocity peaks of
+% the pupil timeseries with the pseudo velocity peaks of the moving
+% target. While we cannot build a proper timeseries for the moving target,
+% we know that the target moves only within a defined interval [minFixTime
+% maxFixTime]. This inteval is hardcoded in the gazeCalibration function.
+
+if ~dotTimesRecorded
+    try
+        % extract target velocity peaks and valleys
+        targetVelocityPeaks = diff(sqrt((targetPPX_X).^2 + (targetPPX_Y).^2));
+        
+%         % extract pupil position peaks
+%         [~, pPeaksIDX] = findpeaks(pupilPosition,'MinPeakHeight',std(pupilPosition),'MinPeakDistance',60);
+% %         invertedPupilPosition = - pupilPosition;
+% %         [~, pValleysIDX] = findpeaks(invertedPupilPosition,'MinPeakHeight',std(pupilPosition),'MinPeakDistance',30);
+% %         
+%         % build pupil position peak array
+%         pupilPositionPeakIDX = pPeaksIDX;%sort([pPeaksIDX; pValleysIDX]);
+%         pupilPositionPeaks = pupilPosition(pupilPositionPeakIDX);
+        
+        
+        % extract pupil velocity peaks and valleys indexes
+        [~, vPeaksIDX] = findpeaks(pupilVelocity,'MinPeakHeight',std(pupilVelocity),'MinPeakDistance',p.Results.saccadeDistance);
+        invertedPupilVelocity = - pupilVelocity;
+        [~, vValleysIDX] = findpeaks(invertedPupilVelocity,'MinPeakHeight',std(pupilVelocity),'MinPeakDistance',p.Results.saccadeDistance);
+        
+        % build pupil velocity peak array
+        pupilVelocityPeakIDX = sort([vPeaksIDX; vValleysIDX]);
+        
+        % find and remove big short saccades that survived filtering
+        saccades = find (diff(pupilVelocityPeakIDX) < p.Results.saccadeDistance);
+        
+        % we define a saccade as 3 peaks too close to each other. In that
+        % case, the "landing peak" (the last) is the one to be preserved
+        if ~isempty(saccades)
+            pupilVelocityPeakIDX(saccades) = [];
+        end
+        
+        % get the true peaks
+        pupilVelocityPeaks = pupilVelocity(pupilVelocityPeakIDX);
+        
+        % cross correlate the peak arrays: the delay index is onset of the
+        % second target.
+        [r,lag]= xcorr(pupilVelocityPeaks,targetVelocityPeaks);
+        [~,I]= max(abs(r));
+        delay = lag(I);
+        firstTargetOnsetIDX = pupilVelocityPeakIDX(delay-1);
+        lastTargetOnsetIDX = pupilVelocityPeakIDX((delay-1)+length(targetVelocityPeaks));
+        
+        % refine the tail of the pupilPosition, cropping it when the fixation
+        % reasonably ends.
+        pupilPositionTail = pupilPosition(lastTargetOnsetIDX:end);
+        
+        % get an estimate of the previous targets duration on screen
+        averageTargetDur = round(mean(diff(pupilVelocityPeakIDX(delay:end))));
+        
+        % compute a moving std for the pupilPosition tail and select the most
+        % stable window
+        [~,minWindowIDX] = min(movstd(pupilPositionTail,averageTargetDur));
+        
+        tailIDX = round(minWindowIDX+(averageTargetDur/2));
+        
+        % finally, get glint and pupil timeseries
+        pupil.X = pupil.X(firstTargetOnsetIDX:lastTargetOnsetIDX+tailIDX);
+        pupil.Y = pupil.Y(firstTargetOnsetIDX:lastTargetOnsetIDX+tailIDX);
+        glint.X = rawGlintData.glintData.X(firstTargetOnsetIDX:lastTargetOnsetIDX+tailIDX);
+        glint.Y = rawGlintData.glintData.Y(firstTargetOnsetIDX:lastTargetOnsetIDX+tailIDX);
+        
+        % also, record the estimated target duration in seconds
+        tarDurFrames = [diff(pupilVelocityPeakIDX(delay:end)); tailIDX-lastTargetOnsetIDX];
+            gazeCalData.fixDurationSec = tarDurFrames ./p.Results.frameRate;
+    catch ME
+        error ('Automatic alignement failed. Please check the calibration data manually.')
+        figure
+        plot(pupilPosition)
+        title('Raw Pupil position timeseries')
+        xlabel('Frames')
+        ylabel('Position')
+        rethrow (ME)
+    end
+end
 
 
+%% Get mean pupil and glint position for each target fixation
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-%%  add meta fields and save results
-gazeCalData.meta = p.Results;
-gazeCal.meta.rawPupil = rawPupilData;
-gazeCal.meta.rawGlint = rawGlintData;
-
-
-
-
-
-
-
-
-
-%% FROM OLD FUNCTION
-
+%%%%%%%%%%%%%%%%%%%%% FROM OLD FUNCTION %%%%%%%%%%%%%%%%%%%%%%%
 
 % % remove some of the boundaries
 removeFrames = round(targetDurFrames ./10);
@@ -285,4 +350,25 @@ for ct = 1 :9
     meanG.X(ct) = movMeanG.X(Xidx);
     meanG.Y(ct) = movMeanG.Y(Xidx);
 end
+
+
+
+
+
+
+
+
+
+
+
+%%  add meta fields and save results
+gazeCalData.meta = p.Results;
+gazeCal.meta.rawPupil = rawPupilData;
+gazeCal.meta.rawGlint = rawGlintData;
+
+% save results
+save (gazeDataFileName , 'gazeCalData')
+
+end % main function
+
 
