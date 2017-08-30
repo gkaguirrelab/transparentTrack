b6b7b39cb71eff398f3592d2b1532df056eb4c00
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2017-08-30 13:03:15 -0400

    deriveCalibrationMatrix is complete
    - added fminsearch step
    - added save step
    - added more documentation

--- code/deriveCalibration/calcCalibrationMatrix.m
+++ code/deriveCalibration/calcCalibrationMatrix.m
@@ -1,4 +1,4 @@
-function calcCalibrationMatrix (gazeDataFileName,calMatrixFileName,varargin)
+function calcCalibrationMatrix (gazeDataFileName,gazeCalParamsFileName,varargin)
 
 % calcCalibrationMatrix (gazeDataFileName,calMatrixFileName)
 % 
@@ -27,6 +27,30 @@ function calcCalibrationMatrix (gazeDataFileName,calMatrixFileName,varargin)
 % coordinates to 3-D screen coordinates, where the 3rd dimension is the
 % distance between the observer and the screen.
 %
+% OUTPUTS: (saved to file)
+%   gazeCalibration: struct containing the calibration matrix and the
+%       perspective correction value to calibrate raw data as shown above.
+% 
+% INPUTS:
+%   gazeDataFileName: name of the mat file containing the gaze calibration
+%       data.
+%   gazeCalParamsFileName: name of the mat file to save the calibration
+%       params.
+% 
+% Optional params:
+%   fminsearchCalls: number of iteration for fminseach, changing the
+%       tolerance.
+%
+% Optional key/value pairs (display and I/O)
+%  'verbosity' - level of verbosity. [none, full]
+%
+% Options (environment)
+%   tbSnapshot - the passed tbSnapshot output that is to be saved along
+%      with the data
+%   timestamp / username / hostname - these are automatically derived and
+%      saved within the p.Results structure.
+% 
+% 
 % 
 % MORE ABOUT CALCULATING THE CALIBRATION MATRIX
 % 
@@ -78,10 +102,10 @@ p = inputParser; p.KeepUnmatched = true;
 
 % Required
 p.addRequired('gazeDataFileName',@ischar);
-p.addRequired('calMatrixFileName',@ischar);
+p.addRequired('gazeCalParamsFileName',@ischar);
 
 % Optional analysis parameters
-
+p.addParameter('fminsearchCalls',20, @isnumeric);
 
 % Optional display and I/O parameters
 p.addParameter('verbosity','none', @ischar);
@@ -93,7 +117,7 @@ p.addParameter('username',char(java.lang.System.getProperty('user.name')),@ischa
 p.addParameter('hostname',char(java.net.InetAddress.getLocalHost.getHostName),@ischar);
 
 % parse
-p.parse(gazeDataFileName, calMatrixFileName, varargin{:})
+p.parse(gazeDataFileName, gazeCalParamsFileName, varargin{:})
 
 
 %% load gaze data
@@ -108,12 +132,90 @@ clear gazeData
 
 %% Estimate the relative perspective correction factor
 
+Rpc = calcRpc(targets,pupil,glint,viewingDistance);
 
-
+%% Define apparent gaze vector and apply perspective correction (will be used in the fminsearch)
 % theoretically, the Z term of the apparent gaze should include the
-% distance between the pupil center and the center of the corneal
-% curvature in pixels (is ranges between 50-200). Given the subsequent steps of dividing by the Rpc and of
-% fminsearch, we can set distancePupilCornea/Rpc as 1 without loss of
-% generality.
+% distance between the pupil center and the center of the corneal curvature
+% in pixels (is ranges between 50-200), and should look like this:
+% 
+%  apparentGaze = 1/Rpc *[(Px -Gx); (Py -Gy); distPupilCornea - sqrt((Px -Gx)^2 +(Py -Gy)^2)];
+% 
+% Given the subsequent fminsearch step, we can set distancePupilCornea/Rpc
+% as 1 without loss of generality, so that:
+
+%  apparentGaze = [(pX-gX)/Rpc; (pY-gY)/Rpc; (1 - sqrt(((pX-gX)/Rpc)^2 + ((pY-gY)/Rpc)^2))]
+
+%% solve minimization problem
+% initialize the matrix
+X = [...
+    1 0 0 0 ...
+    0 1 0 0 ...
+    0 0 0 0 ...
+    0 0 0 1 ...
+    ];
+
+% exclude nan targets, if any
+pupil.X = pupil.X(~isnan(targets.X));
+pupil.Y = pupil.Y(~isnan(targets.X));
+glint.X = glint.X(~isnan(targets.X));
+glint.Y = glint.Y(~isnan(targets.X));
+targets.X = targets.X(~isnan(targets.X));
+targets.Y = targets.Y (~isnan(targets.X));
+
+% Loop through calls to fminsearch, changing tolerance
+for i=1:p.Results.fminsearchCalls
+    options = optimset('Display','off','MaxFunEvals', 10000,...
+        'MaxIter', 10000, 'TolX',10^(-i/2),'TolFun',10^(-i/2),'PlotFcns',[] );
+    [X, f] = fminsearch(@(param) ...
+        errfun(param,pupil,glint,targets,viewingDistance,Rpc),...
+        X, options);
+    disp(['RSS error: ',num2str(f)])
+end
+
+% make the calibration matrix
+calMat = [X(1:4); X(5:8); X(9:12); X(13:16)];
+
+%% save out calibration matrix, Rpc and metadata in a file
+gazeCalibration.calMatrix = calMat;
+gazeCalibration.Rpc = Rpc;
+gazeCalibration.meta = p.Results;
+gazeCalibration.meta.RSSerror = f;
+
+save(gazeCalParamsFileName,'gazeCalibration')
+
+end % main function
 
 
+%% error function for fminsearch
+function errtot = errfun(param, pupil, glint, targets, viewingDistance, Rpc)
+
+err = nan(1,length(targets(:,1)));
+CalMatrix = [param(1:4); param(5:8); param(9:12); param(13:16)];
+
+% minimize error for each target
+for i = 1:length(targets.X)
+    pX = pupil.X(i);
+    pY = pupil.Y(i);
+    gX = glint.X(i);
+    gY = glint.Y(i);
+    x = targets.X(i);
+    y = targets.Y(i);
+    z = viewingDistance;
+    
+    % calibrated apparent gaze vector in homogeneous coordinates
+    aXYZW = CalMatrix * [(pX-gX)/Rpc; (pY-gY)/Rpc; (1 - sqrt(((pX-gX)/Rpc)^2 + ((pY-gY)/Rpc)^2)); 1];
+    
+    % target vector in homogeneous coordinates
+    oXYZW = [x; y; z; 1];
+    
+    % distance gaze - target in 3-D
+    errXYZ = (aXYZW(1:3)/aXYZW(4)) - (oXYZW(1:3)/oXYZW(4));
+    
+    % error for this target
+    err(i) = nansum(errXYZ.^2);
+end
+
+% total error
+errtot = nansum(err.^2);
+end % error function
