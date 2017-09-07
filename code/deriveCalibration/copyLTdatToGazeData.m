1a73e3d5024bb82d6f5847d472d9199c6a0e66ba
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2017-09-07 17:15:02 -0400

    New functions
    Yet to be debugged

---
+++ code/deriveCalibration/copyLTdatToGazeData.m
@@ -0,0 +1,64 @@
+function copyLTdatToGazeData(LTdatFileName,gazeDataFileName,varargin)
+% copyLTdatToGazeData(LTdatFileName,gazeDataFileName)
+
+% this function is to use for gaze calibration data collected with the
+% LiveTrack device. If the user wishes to use the livetrack data for
+% calibration, this routine export the data in a standard format for later
+% calibration steps.
+
+%% input parser
+
+p = inputParser; p.KeepUnmatched = true;
+
+% Required
+p.addRequired('LTdatFileName',@ischar);
+p.addRequired('gazeDataFileName',@ischar);
+
+% Optional analysis parameters
+p.addParameter('viewingDistance', 1065, @isnumeric)
+p.addParameter('units', 'mm', @ischar)
+
+% Optional display and I/O parameters
+p.addParameter('verbosity','none', @ischar);
+p.addParameter('showFigures',false, @islogical);
+
+% Environment parameters
+p.addParameter('tbSnapshot',[],@(x)(isempty(x) | isstruct(x)));
+p.addParameter('timestamp',char(datetime('now')),@ischar);
+p.addParameter('username',char(java.lang.System.getProperty('user.name')),@ischar);
+p.addParameter('hostname',char(java.net.InetAddress.getLocalHost.getHostName),@ischar);
+
+% parse
+p.parse(LTdatFileName, gazeDataFileName, varargin{:})
+
+
+%% load LTdat file
+LTdata = load(LTdatFileName);
+
+
+%% read data from ltDatFile and store them in gazeCalData
+
+% get target location
+gazeCalData.targets.X     = LTdata.targets(:,1); % mm on screen, screen center = 0
+gazeCalData.targets.Y     = LTdata.targets(:,2); % mm on screen, screen center = 0
+
+
+% get pupil and glint location for each fixation
+gazeCalData.pupil.X = LTdata.pupil(:,1);
+gazeCalData.pupil.Y = LTdata.pupil(:,2);
+gazeCalData.glint.X = LTdata.glint(:,1);
+gazeCalData.glint.Y = LTdata.glint(:,2);
+
+% add viewing distance
+gazeCalData.viewingDistance = p.Results.viewingDistance;
+
+% add metadata
+gazeCalData.meta = p.Results;
+
+% add an additional field to indicate that we are using livetrack data
+gazeCalData.meta.usingLiveTrackData = true;
+
+% save results
+save (gazeDataFileName , 'gazeCalData')
+
+end % main function
