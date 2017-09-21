a1e7b40280ff44ca5fa888e70db5c76312753ebf
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2017-09-21 16:54:18 -0400

    Removed world coordinates conversion
    As it was ambiguous and not crucial in terms of Gaze Calibration, AS
    LONG AS EVERYTHING IS TRACKED IN THE SAME SYSTEM.

--- code/processVideos/fitPupilPerimeter.m
+++ code/processVideos/fitPupilPerimeter.m
@@ -27,15 +27,15 @@ function [pupilData] = fitPupilPerimeter(perimeterFileName, pupilFileName, varar
 % pixels image in intrisic coordinates will be represented in a grid with
 % xlim = [0.5 3.5] and ylim = [0.5 3.5], with the origin being the top left
 % corner of the image. This is done to facilitate the handling of images in
-% many of the built-in image processing functions. We are interested in
+% many of the built-in image processing functions. This does not constitute
+% an issue for the following steps of the routine, as long as all pupil
+% data coordinate system/units are kept consistent. If interested in
 % returning the results in "world units", which in our case correspond to
 % the cartesian system having the origin in the top left corner of the
 % frame and, referring to the above example, xlim = [0 3] and ylim = [0 3],
-% with the assumption of using square pixels. To apply this conversion, the
-% last stage before saving the pupil data is to subtract 0.5 pixels from
-% every X transparent coordinate and add 0.5 pixel to every Y transparent
-% coordinate.
-% REF for intrinsic coordinates explaination: 
+% the operation to do is to  subtract 0.5 pixels from each X and each Y
+% transparent coordinate.
+% REF for intrinsic coordinates explaination:
 % https://blogs.mathworks.com/steve/2013/08/28/introduction-to-spatial-referencing/
 %
 % NOTES REGARDING USE OF PARALLEL POOL
@@ -352,11 +352,6 @@ else
         end % try catch
     end % loop over frames
     
-    % convert to world coordinates  (if this is the last stage)
-    if p.Results.skipPupilBayes
-        loopVar_pInitialFitTransparent(:,1) = loopVar_pInitialFitTransparent(:,1) - 0.5;
-        loopVar_pInitialFitTransparent(:,2) = loopVar_pInitialFitTransparent(:,2) + 0.5;
-    end
     % gather the loop vars into the ellipse structure    
     pupilData.pInitialFitTransparent = loopVar_pInitialFitTransparent;
     pupilData.pInitialFitHessianSD = loopVar_pInitialFitHessianSD;
@@ -526,17 +521,6 @@ if ~p.Results.skipPupilBayes
     end % loop over frames to calculate the posterior
     
     
-    %% convert to world coordinates
-        % convert to world coordinates
-        loopVar_pPriorMeanTransparent(:,1) = loopVar_pPriorMeanTransparent(:,1) - 0.5;
-        loopVar_pPriorMeanTransparent(:,2) = loopVar_pPriorMeanTransparent(:,2) + 0.5;
-        loopVar_pPosteriorMeanTransparent(:,1) = loopVar_pPosteriorMeanTransparent(:,1) - 0.5;
-        loopVar_pPosteriorMeanTransparent(:,2) = loopVar_pPosteriorMeanTransparent(:,2) + 0.5;
-        if ~ p.Results.skipInitialPupilFit
-            pupilData.pInitialFitTransparent(:,1) = pupilData.pInitialFitTransparent(:,1) - 0.5;
-            pupilData.pInitialFitTransparent(:,2) = pupilData.pInitialFitTransparent(:,2) + 0.5;
-        end
-    
     %% Clean up and save the fit results
     
     % gather the loop vars into the ellipse structure
@@ -548,7 +532,7 @@ if ~p.Results.skipPupilBayes
     
     % add a meta field with analysis details
     pupilData.meta = p.Results;
-    pupilData.meta.coordinatesSystem = 'worldCoordinates';
+    pupilData.meta.coordinatesSystem = 'intrinsicCoordinates(pixels)';
     
     % save the ellipse fit results if requested
     if ~isempty(p.Results.pupilFileName)
