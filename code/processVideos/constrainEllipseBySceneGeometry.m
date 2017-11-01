5285de7871602517e75fea70ddd6f8277f4d429b
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2017-11-01 17:49:25 -0400

    bugfixes

--- code/processVideos/constrainEllipseBySceneGeometry.m
+++ code/processVideos/constrainEllipseBySceneGeometry.m
@@ -1,4 +1,4 @@
-function [eccentricity, theta] = constrainEllipseBySceneGeometry (ellipseCenter,sceneGeometryFileName, varargin)
+function [eccentricity, theta] = constrainEllipseBySceneGeometry (ellipseCenter,sceneGeometry, varargin)
 % [eccentricity, theta] = constrainEllipseBySceneGeometry (ellipseCenter,sceneGeometryFileName)
 %
 % This function returns the expected ecceentricity and tilt for an ellipse,
@@ -17,12 +17,12 @@ function [eccentricity, theta] = constrainEllipseBySceneGeometry (ellipseCenter,
 % ellipseCenter - [X Y] coordinate for the ellipse center. Note that
 %   passing the full parametrization of a transparent ellipse will work as
 %   well.
-% sceneGeometryFileName - name of the scene geometry file
+% sceneGeometryFileName - struct with scene geometry
 % 
 % Optional Input (analysis)
-% distanceFromSceneRangePx - [minDistPx maxDistPx] estimate of the distance
-%   of the eye from the scene plane (i.e. from the camera). This must be in
-%   pixel units.
+% distanceFromSceneRangePx - [distInPx] or [minDistPx maxDistPx] estimate
+%   of the distance of the eye from the scene plane (i.e. from the camera).
+%   This must be in pixel units.
 
 
 %% input parser
@@ -30,35 +30,42 @@ p = inputParser; p.KeepUnmatched = true;
 
 % required input
 p.addRequired('ellipseCenter',@isnumeric);
-p.addRequired('sceneGeometryFileName',@isstr);
+p.addRequired('sceneGeometry',@isstruct);
 
 % optional input
-p.addParameter('distanceFromSceneRangePx', [],@isnumeric)
+p.addParameter('distanceFromScenePx', [],@isnumeric)
 
 %parse
-p.parse(ellipseCenter, sceneGeometryFileName, varargin{:})
-%% load scene geometry
-load(sceneGeometryFileName)
+p.parse(ellipseCenter, sceneGeometry, varargin{:})
+
 
 %% derive rotation angles and reconstruct eccentricity and theta
-if isempty (p.Results.distanceFromSceneRangePx)
+if isempty (p.Results.distanceFromScenePx)
     pupilAzi = atand((ellipseCenter(1)-sceneGeometry.eyeball.X)/sceneGeometry.eyeball.Z);
     pupilEle = atand((ellipseCenter(2)-sceneGeometry.eyeball.Y)/sceneGeometry.eyeball.Z);
     
     reconstructedTransparentEllipse = pupilProjection_fwd(pupilAzi, pupilEle, [sceneGeometry.eyeball.X sceneGeometry.eyeball.Y sceneGeometry.eyeball.Z]);
     eccentricity = reconstructedTransparentEllipse(4);
     theta = reconstructedTransparentEllipse(5);
-else
+    
+elseif length (p.Results.distanceFromScenePx) == 1
+    pupilAzi = atand((ellipseCenter(1)-sceneGeometry.eyeball.X)/p.Results.distanceFromScenePx);
+    pupilEle = atand((ellipseCenter(2)-sceneGeometry.eyeball.Y)/p.Results.distanceFromScenePx);
+    reconstructedTransparentEllipse = pupilProjection_fwd(pupilAzi, pupilEle, [sceneGeometry.eyeball.X sceneGeometry.eyeball.Y p.Results.distanceFromScenePx]);
+    eccentricity = reconstructedTransparentEllipse(4);
+    
+    theta = reconstructedTransparentEllipse(5);
+elseif length (p.Results.distanceFromScenePx) == 2
     % calculate lower bound values for eccentricity
-    minPupilAzi = atand((ellipseCenter(1)-sceneGeometry.eyeball.X)/p.Results.distanceFromSceneRangePx(1));
-    minPupilEle = atand((ellipseCenter(2)-sceneGeometry.eyeball.Y)/p.Results.distanceFromSceneRangePx(1));
-    minReconstructedTransparentEllipse = pupilProjection_fwd(minPupilAzi, minPupilEle, [sceneGeometry.eyeball.X sceneGeometry.eyeball.Y p.Results.distanceFromSceneRangePx(1)]);
+    minPupilAzi = atand((ellipseCenter(1)-sceneGeometry.eyeball.X)/p.Results.distanceFromScenePx(1));
+    minPupilEle = atand((ellipseCenter(2)-sceneGeometry.eyeball.Y)/p.Results.distanceFromScenePx(1));
+    minReconstructedTransparentEllipse = pupilProjection_fwd(minPupilAzi, minPupilEle, [sceneGeometry.eyeball.X sceneGeometry.eyeball.Y p.Results.distanceFromScenePx(1)]);
     eccentricity(1) = minReconstructedTransparentEllipse(4);
     
     % calculate upper bound values fpr eccentricity
-    maxPupilAzi = atand((ellipseCenter(1)-sceneGeometry.eyeball.X)/p.Results.distanceFromSceneRangePx(2));
-    maxPupilEle = atand((ellipseCenter(2)-sceneGeometry.eyeball.Y)/p.Results.distanceFromSceneRangePx(2));
-    maxReconstructedTransparentEllipse = pupilProjection_fwd(maxPupilAzi, maxPupilEle, [sceneGeometry.eyeball.X sceneGeometry.eyeball.Y p.Results.distanceFromSceneRangePx(2)]);
+    maxPupilAzi = atand((ellipseCenter(1)-sceneGeometry.eyeball.X)/p.Results.distanceFromScenePx(2));
+    maxPupilEle = atand((ellipseCenter(2)-sceneGeometry.eyeball.Y)/p.Results.distanceFromScenePx(2));
+    maxReconstructedTransparentEllipse = pupilProjection_fwd(maxPupilAzi, maxPupilEle, [sceneGeometry.eyeball.X sceneGeometry.eyeball.Y p.Results.distanceFromScenePx(2)]);
     eccentricity(2) = maxReconstructedTransparentEllipse(4);
     
     % calculate the tilt
