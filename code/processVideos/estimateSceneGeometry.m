054ff65a04dab1aeb8a780e3313d968b3c3f9dfd
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2017-11-02 16:36:49 -0400

    distanceToCandidateEyeballCenter is now a subfunction
    since it used only by estimateSceneGeometry.

--- code/processVideos/estimateSceneGeometry.m
+++ code/processVideos/estimateSceneGeometry.m
@@ -106,7 +106,7 @@ bestFitCoP = fmincon(errorFunc, x0);
 %% if so requested, plot the results of the CoP estimation
 
 if p.Results.plotResults
-    [distances, ellipsesCOPs] = distanceToCandidateEyeBallCenter(ellipses,bestFitCoP);
+    [distances, ellipsesCOPs] = distanceToCandidateEyeBallCenter(ellipses,bestFitCoP,p.Results.eyeballRadiusInPixels,'orthogonalProjection', p.Results.orthogonalProjection);
     figure
     plot(ellipsesCOPs(:,1), ellipsesCOPs(:,2), '.')
     hold on
@@ -122,7 +122,7 @@ end
 sceneGeometry.eyeballCenter.X = bestFitCoP(1);
 sceneGeometry.eyeballCenter.Y = bestFitCoP(2);
 sceneGeometry.eyeballCenter.Z = bestFitCoP(3); % meant as the distance from the scene plane
-sceneGeometry.eyeballRadius = bestFitCoP(4);
+sceneGeometry.eyeballRadius = p.Results.eyeballRadiusInPixels;
 sceneGeometry.units = 'pixelsOnTheScenePlane';
 sceneGeometry.meta = p.Results;
 
@@ -131,3 +131,68 @@ if ~isempty(sceneGeometryFileName)
 end
 
 end % function
+
+
+function [distances, ellipsesCoRs] = distanceToCandidateEyeBallCenter(ellipses, candidateEyeballCenter, eyeballRadiusInPixels, varargin)
+%  [distances,ellipsesCoPs] = distanceToCandidateEyeBallCenter(ellipses,candidateEyeball,varargin)
+%
+% this function finds the distance of candidate center of rotation for
+% each ellipses and the center of projection on the scene. Ellipses and
+% candidate Eyeball must be in the same units.
+% (e.g. Pixels)
+%
+% input 
+%  ellipses - set of ellipses in transparent form.
+%  candidateEyeballCenter -[X Y Z] coordinates of the center. If
+%       orthogonal projection, the Z coordinate (distance from the scene plane,
+%       will be set to zero).
+%  eyeballRadiusInPixels - radius of the eyeball in pixel
+% 
+%  Output
+%     distances
+%     ellipsesCoRs
+%% input parser
+p = inputParser; p.KeepUnmatched = true;
+
+% required input
+p.addRequired('ellipses',@isnumeric);
+p.addRequired('candidateEyeballCenter',@isnumeric);
+p.addRequired('eyeballRadiusInPixels',@isnumeric);
+
+% Analysis parameters
+p.addParameter('orthogonalProjection',true,@islogical);
+
+% parse
+p.parse(ellipses, candidateEyeballCenter, eyeballRadiusInPixels, varargin{:})
+
+if p.Results.orthogonalProjection
+    
+%% initialize variables
+distances = nan(size(ellipses,1),2);
+ellipsesCoRs = nan(size(ellipses,1),3);
+
+%% loop through ellipses, find all distances and pick center of projection
+for ii = 1:size(ellipses,1)
+    if ~any(isnan(ellipses(ii,:)))
+        % find candidates Center of rotation
+        ellipseCoRCandidates = findCandidatesEyeball(ellipses(ii,:), eyeballRadiusInPixels, 'orthogonalProjection', p.Results.orthogonalProjection);
+        
+        % find eucledian distance for each ellipse COP
+        for jj = 1:size(ellipseCoRCandidates,1)
+            distances(ii,jj) = sqrt((candidateEyeballCenter(1) - ellipseCoRCandidates(jj,1))^2 + (candidateEyeballCenter(2) - ellipseCoRCandidates(jj,2))^2 + (candidateEyeballCenter(3) - ellipseCoRCandidates(jj,3))^2);
+        end
+        % select ellipseCoR with the min distance from the scene CoR and store
+        % it
+        [~,minDistIDX] =min(distances(ii,:));
+        ellipsesCoRs(ii,:) = ellipseCoRCandidates(minDistIDX,:);
+    else
+        continue
+    end
+end
+
+distances = min(distances');
+
+else
+    % Dev PLACEHOLDER - develop perspective correction case
+end
+end %function
