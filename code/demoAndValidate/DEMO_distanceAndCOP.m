d2c4f81d3ef54d2907d3bfc03e5721a05966fdf7
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2017-11-03 11:01:37 -0400

    Edits to the DEMO
    Demo produces now a video with the reconstructed ellipses

--- code/demoAndValidate/DEMO_distanceAndCOP.m
+++ code/demoAndValidate/DEMO_distanceAndCOP.m
@@ -53,11 +53,88 @@ title('timeseries for the sin of the thetas') % for easier comparison
 legend('transparentEllipses tilt angles','constrained tilt angles')
 ylabel('sin(theta)')
 
-% %% use the derived eccentricity and thetas to constrain the ellipse fit
-% % constrained pupil fit here
-% 
-% %% project the constrained fitted pupil file back in 3d to derive azi and elevation 
-% % need to test this
-% [reconstructedPupilAzi, reconstructedPupilEle, reconstructedPupilRadius] = pupilProjection_inv(transparentEllipse,centerOfProjection);
-% 
-% %% do the smoothing in 3d
+%% use the derived eccentricity and thetas to reconstruct the ellipses
+% we take the initalFit ellipses, maintain the X Y area values and
+% substitue the constraint eccentricity and theta. Then, we plot this on
+% top of the original video to see how the constrained fit looks like.
+% (while it won't be perfect, we hope that it is not too far off)
+%
+constrainedEllipses = [ellipses(:,1:3) eccentricity theta];
+grayVideoName = '~/Desktop/eyeTrackingDEMO/TOME_processing/session2_spatialStimuli/TOME_3020/050517/EyeTracking/GazeCal01_gray.avi';
+constrainedEllipsesVideoName  = '~/Desktop/eyeTrackingDEMO/TOME_processing/session2_spatialStimuli/TOME_3020/050517/EyeTracking/GazeCal01_constrainedEllipses.avi';
+
+% read video file into memory
+videoInObj = VideoReader(grayVideoName);
+
+% get number of frames
+nFrames = floor(videoInObj.Duration*videoInObj.FrameRate);
+nFrames = 1000;
+
+% get video dimensions
+videoSizeX = videoInObj.Width;
+videoSizeY = videoInObj.Height;
+% read the video into memory
+for ii = 1:nFrames
+    thisFrame = readFrame(videoInObj);
+    sourceVideo(:,:,ii) = rgb2gray (thisFrame);
+end
+% close the video object
+clear videoInObj
+
+% prepare the outputVideo
+outputVideo=zeros(videoSizeY,videoSizeX,3,nFrames,'uint8');
+
+for ii = 1:nFrames
+    
+    % Create a figure
+    frameFig = figure( 'Visible', 'off');
+    
+    % show the initial frame
+    imshow(squeeze(sourceVideo(:,:,ii)), 'Border', 'tight');
+    hold on
+    
+    if sum(isnan(constrainedEllipses(ii,:)))==0
+        % build ellipse impicit equation
+        pFitImplicit = ellipse_ex2im(ellipse_transparent2ex(constrainedEllipses(ii,:)));
+        fh=@(x,y) pFitImplicit(1).*x.^2 +pFitImplicit(2).*x.*y +pFitImplicit(3).*y.^2 +pFitImplicit(4).*x +pFitImplicit(5).*y +pFitImplicit(6);
+        % superimpose the ellipse using fimplicit or ezplot
+        if exist('fimplicit','file')==2
+            fimplicit(fh,[1, videoSizeX, 1, videoSizeY],'Color', 'red','LineWidth',1);
+            set(gca,'position',[0 0 1 1],'units','normalized')
+            axis off;
+        else
+            plotHandle=ezplot(fh,[1, videoSizeX, 1, videoSizeY]);
+            set(plotHandle, 'Color', 'red')
+            set(plotHandle,'LineWidth',1);
+        end
+    end
+    
+    % Save the frame and close the figure
+    tmp=getframe(frameFig);
+    outputVideo(:,:,:,ii)=tmp.cdata;
+    close(frameFig);
+    
+end
+
+% Create a color map
+cmap = [linspace(0,1,256)' linspace(0,1,256)' linspace(0,1,256)'];
+cmap(1,:)=[1 0 0];
+cmap(2,:)=[0 1 0];
+cmap(3,:)=[0 0 1];
+cmap(4,:)=[1 1 0];
+cmap(5,:)=[0 1 1];
+cmap(6,:)=[1 0 1];
+
+% write the outputVideo to file
+videoOutObj = VideoWriter(constrainedEllipsesVideoName,'Indexed AVI');
+videoOutObj.FrameRate = 60;
+videoOutObj.Colormap = cmap;
+open(videoOutObj);
+
+% loop through the frames and save them
+for ii=1:nFrames
+    indexedFrame = rgb2ind(squeeze(outputVideo(:,:,:,ii)), cmap, 'nodither');
+    writeVideo(videoOutObj,indexedFrame);
+end
+% close the videoObj
+clear videoOutObj
