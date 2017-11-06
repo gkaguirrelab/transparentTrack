03f75954c26d36e045c8133de3501a5dd21288cb
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2017-11-06 15:05:56 -0500

    Added 2 new keyValue pairs
    skipStageByNumber - an array of function calls numbers to be skipped
    during execution of the pipeline.
    displayAvailableStages - displays numbered list of the available
    stages, to be used as reference for skipStageByNumber. Note that if
    this is set to true, no analysis will be performed.

--- code/processVideos/runVideoPipeline.m
+++ code/processVideos/runVideoPipeline.m
@@ -29,7 +29,7 @@ function runVideoPipeline( pathParams, varargin )
 %           dataOutputDirFull - full path to the directory where the files
 %               that result from processing will be written
 %           runName - the stem name for the source and subsequent output
-%               files. 
+%               files.
 %       This structure may have other fields defined that are used in
 %       routines prior to this one to assemble the full paths to the source
 %       and output dirs.
@@ -42,6 +42,11 @@ function runVideoPipeline( pathParams, varargin )
 %      production of the fit video.
 %   skipStage - a cell array of function calls to be skipped during
 %      execution of the pipeline.
+%   skipStageByNumber - an array of function calls numbers to be skipped during
+%      execution of the pipeline.
+%   displayAvailableStages - displays numbered list of the available
+%       stages, to be used as reference for skupStageByNumber. Note that if
+%       this is set to true, no analysis will be perfomed.
 %   rawVideoSuffix - cell array of strings that contain possible suffixes
 %       of raw video files to be processed. The routine will search for
 %       raw videos sequentially in the cell array until it finds a match
@@ -58,7 +63,7 @@ function runVideoPipeline( pathParams, varargin )
 %           LiveTrackWithVTOP_eyeNoIris - the standard, but skipping
 %           fitting of the iris bounds as this is under development.
 %   customFunCalls - A cell array of functional calls and key values that
-%       can be passed in lieu of hard-coding a videoTypeChoice set here. 
+%       can be passed in lieu of hard-coding a videoTypeChoice set here.
 %       This is used in concert with passing 'custom' to videoTypeChoice.
 %   catchErrors - controls if the function calls take place within a
 %       try-catch block. If set to true (the default) then the routine will
@@ -79,6 +84,8 @@ p.addRequired('pathParams',@isstruct);
 % optional input
 p.addParameter('lastStage', 'makePupilFitVideo', @ischar);
 p.addParameter('skipStage', {}, @iscell);
+p.addParameter('skipStageByNumber',[], @isnumeric);
+p.addParameter('displayAvailableStages', false, @islogical)
 p.addParameter('rawVideoSuffix', {'_raw.mov' '.mov'}, @iscell);
 p.addParameter('videoTypeChoice', 'LiveTrackWithVTOP_eyeNoIris', @ischar);
 p.addParameter('customFunCalls', {}, @iscell);
@@ -181,7 +188,7 @@ switch p.Results.videoTypeChoice
             '''irisFileName'', irisFileName,' ...
             '''controlFileName'',controlFileName,varargin{:});']...
             };
-        case 'LiveTrackWithVTOP_eyeNoIris'
+    case 'LiveTrackWithVTOP_eyeNoIris'
         funCalls = {...
             'deinterlaceVideo(rawVideoName,grayVideoName, varargin{:});'...
             'findGlint(grayVideoName, glintFileName, varargin{:});'...
@@ -206,53 +213,64 @@ switch p.Results.videoTypeChoice
         error('VideoTypeChoice is not defined')
 end
 
+
 % Grab the function names as the portion of the funCalls that preceed the
 % open parenthesis
 funNames = cellfun(@(x) strtok(x,'('),funCalls,'UniformOutput',false);
 
+% check if just displaying stages in use
+if p.Results.displayAvailableStages
+    display(['Available stages for ' p.Results.videoTypeChoice])
+    for ff = 1:length(funCalls)
+        fprintf ([num2str(ff) ' - ' funNames{ff} '\n'])
+    end
+    return
+end
+
 % Loop through the function calls
 for ff = 1:length(funCalls)
     if ~any(strcmp(p.Results.skipStage,funNames{ff}))
-
-        % check if we are going to catch or throw errors
-        if p.Results.catchErrors
-            % intialize while control
-            success = 0;
-            attempts = 1; % we will attempt to execute the instruction 3 times. If it does not work, the code will eventually break.
-            while ~success
-                try
-                    eval(funCalls{ff});
-                    success = 1;
-                catch ME
-                    warning ('There has been an error during execution. Cleaning matlabprefs.mat and trying again - attempt %d.',attempts)
-                    cleanupMatlabPrefs;
-                    success = 0;
-                    attempts = attempts +1;
-                    if attempts > 3
-                        rethrow(ME)
+        if ~any(p.Results.skipStageByNumber == ff)
+            % check if we are going to catch or throw errors
+            if p.Results.catchErrors
+                % intialize while control
+                success = 0;
+                attempts = 1; % we will attempt to execute the instruction 3 times. If it does not work, the code will eventually break.
+                while ~success
+                    try
+                        eval(funCalls{ff});
+                        success = 1;
+                    catch ME
+                        warning ('There has been an error during execution. Cleaning matlabprefs.mat and trying again - attempt %d.',attempts)
+                        cleanupMatlabPrefs;
+                        success = 0;
+                        attempts = attempts +1;
+                        if attempts > 3
+                            rethrow(ME)
+                        end
                     end
                 end
+                % clear while and if control
+                clear success
+                clear attempts
+            else % we will throw errors
+                eval(funCalls{ff});
+            end % if catchErrors
+            
+            % clear all files (hopefully prevents 'too many files open' error)
+            fclose all ;
+            if strcmp(p.Results.lastStage,funNames{ff})
+                return
             end
-            % clear while and if control
-            clear success
-            clear attempts
-        else % we will throw errors
-            eval(funCalls{ff});
-        end % if catchErrors
-
-        % clear all files (hopefully prevents 'too many files open' error)
-        fclose all ;
-        if strcmp(p.Results.lastStage,funNames{ff})
-            return
-        end
-    end % if we aren't skipping this stage
+        end % if we aren't skipping this stage by number
+    end % if we aren't skipping this stage by name
 end % loop over function calls
 
 end % main function
 
 
 
-function cleanupMatlabPrefs    
+function cleanupMatlabPrefs
 % This small function closes any open parallel pool and removes the
 % matlabprefs.mat file in case it is corrupt.
 % This file can get corrupt while using parallel pools because all of the
