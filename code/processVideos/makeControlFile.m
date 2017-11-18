a1d80eabbd54f31ebfbf6680ed86495a3003c7c1
gkaguirre <gkaguirre@me.com>
2017-11-17 21:57:54 -0500

    Default is to not do parallel processing
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/processVideos/makeControlFile.m
+++ code/processVideos/makeControlFile.m
@@ -129,7 +129,7 @@ p.addParameter('verbosity','none',@ischar);
 % Optional flow control params
 p.addParameter('overwriteControlFile',false,@islogical);
 p.addParameter('nFrames',Inf,@isnumeric);
-p.addParameter('useParallel',true,@islogical);
+p.addParameter('useParallel',false,@islogical);
 p.addParameter('nWorkers',[],@(x)(isempty(x) | isnumeric(x)));
 p.addParameter('tbtbRepoName','transparentTrack',@ischar);
 
