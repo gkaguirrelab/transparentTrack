4a605ca19c3547793f733485cae525c176fa7734
gkaguirre <gkaguirre@me.com>
2017-11-22 07:56:44 -0500

    Tweak unit test
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/demoAndValidate/TEST_invertProjectionModel.m
+++ code/demoAndValidate/TEST_invertProjectionModel.m
@@ -5,12 +5,14 @@ tolerance = 1e-9;
 eyeCenter = [320 240 1500];
 eyeRadius = 150;
 
+rotationMax = 30;
+
 projectionModels = {'pseudoPerspective' 'orthogonal' };
 
 for models = 1:length(projectionModels)
     fprintf(['testing ' projectionModels{models} ' model \n']);
-    for pupilAzimuth = -15:15:15
-        for pupilElevation = -15:15:15
+    for pupilAzimuth = -rotationMax:rotationMax:rotationMax
+        for pupilElevation = -rotationMax:rotationMax:rotationMax
             reconstructedTransparentEllipse = pupilProjection_fwd(pupilAzimuth, pupilElevation, nan, eyeCenter, eyeRadius, projectionModels{models});
             [reconstructedPupilAzi, reconstructedPupilEle, ~] = pupilProjection_inv(reconstructedTransparentEllipse, eyeCenter, eyeRadius, projectionModels{models});
             if abs(reconstructedPupilAzi-pupilAzimuth) > tolerance || abs(reconstructedPupilEle-pupilElevation) > tolerance
