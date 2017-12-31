0c144e9a38e38de26743d598af5cd0526cc728f7
gkaguirre <gkaguirre@me.com>
2017-12-31 10:41:01 -0500

    Create a unit test of the projection fwd/inv model; removed out-dated tests
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/demoAndValidate/TEST_invertProjectionModel.m
+++ code/demoAndValidate/TEST_invertProjectionModel.m
@@ -1,47 +1,32 @@
-%% UNIT TEST OF FORWARD AND INVERSE PUPIL PROJECTION MODELS
+%% UNIT TEST OF FORWARD AND INVERSE PUPIL PROJECTION MODEL
 
-tolerance = 1e-9;
+% Construct a sceneGeometry structure
+sceneGeometry.intrinsicCameraMatrix = [772.5483 0 320; 0 772.5483 240; 0 0 1];
+sceneGeometry.extrinsicRotationMatrix = [1 0 0; 0 -1 0; 0 0 -1];
+sceneGeometry.eyeRadius = 11.29;
+sceneGeometry.extrinsicTranslationVector = [0; 0; 50];
+sceneGeometry.constraintTolerance = 0.02;
 
-eyeCenter = [320 240 1500];
-eyeRadius = 150;
-pupilArea = 1000;
-projectionModels = {'pseudoPerspective' 'orthogonal' };
+azimuths=[];
+elevations=[];
+azimuthErrors=[];
+elevationErrors=[];
+radiusErrors=[];
 
-% %% Conduct some test
-% 
-% % Can we invert the forward projection? We test if the forward projection
-% % ellipsed can be modeled with the sceneGeometry within a 0.1% error on
-% % matching ellipse theta, eccentrivity, and error.
-% for ii=1:length(pupilCenterAzimuths)
-%     [eyeParams, bestMatchEllipseOnImagePlane, constraintViolations(ii)] = ...
-%         pupilProjection_inv(projectedEllipsesOnImagePlane(ii,:), sceneGeometry, 'constraintTolerance', 0.001);
-%     eyeParamError(ii,:)=eyeParams-[pupilCenterAzimuths(ii), pupilCenterElevations(ii), pupilRadii(ii)];
-%     ellipseParamError(ii,:)=bestMatchEllipseOnImagePlane-projectedEllipsesOnImagePlane(ii,:);
-% end
-% fprintf('There were %d / %d constraint violations in the inversion of the forward model \n',sum(constraintViolations),length(pupilCenterAzimuths));
-
-
-% Test if we can recover the azimuth and elevation of the eye after
-% projecting the pupil to the image plane
-allPass = true;
-thetas=[];
-rotationMax = 89;
-for models = 1:length(projectionModels)
-    fprintf(['testing ' projectionModels{models} ' model for eye --> ellipse --> eye \n']);
-    for pupilAzimuth = -rotationMax:1:rotationMax
-        for pupilElevation = -rotationMax:rotationMax:rotationMax
-            reconstructedTransparentEllipse = pupilProjection_fwd(pupilAzimuth, pupilElevation, pupilArea, eyeCenter, eyeRadius, projectionModels{models});
-            thetas=[thetas reconstructedTransparentEllipse(5)];
-            [reconstructedPupilAzi, reconstructedPupilEle, reconstructedPupilArea] = pupilProjection_inv(reconstructedTransparentEllipse, eyeCenter, eyeRadius, projectionModels{models});
-            if abs(reconstructedPupilAzi-pupilAzimuth) > tolerance || abs(reconstructedPupilEle-pupilElevation) > tolerance || abs(reconstructedPupilArea-pupilArea) > tolerance
-                fprintf('Failed inversion check for azimuth %d, elevation %d \n',pupilAzimuth,pupilElevation);
-                fprintf('   reconstruced azimuth %0.3f, reconstructed elevation %0.3f, reconstructed pupil area %0.3f \n',reconstructedPupilAzi,reconstructedPupilEle,reconstructedPupilArea);
-                allPass=false;
-            end
-        end
+for thisAzimuth = -35:5:35
+    for thisElevation = -25:5:25
+        azimuths=[azimuths thisAzimuth];
+        elevations=[elevations thisElevation];
+            eyeParams=[thisAzimuth,thisElevation,2];
+            pupilEllipseOnImagePlane = pupilProjection_fwd(eyeParams, sceneGeometry);
+            reconstructedEyeParams = pupilProjection_inv(pupilEllipseOnImagePlane, sceneGeometry);
+            eyeParamError = eyeParams-reconstructedEyeParams;
+        azimuthErrors=[azimuthErrors eyeParamError(1)];
+        elevationErrors=[elevationErrors eyeParamError(2)];
+        radiusErrors=[radiusErrors eyeParamError(3)];
     end
 end
 
-if allPass
-    fprintf(['All inversion tests passed\n']);
-end
+fprintf('The largest azimuth error is: %f \n',max(azimuthErrors));
+fprintf('The largest elevation error is: %f \n',max(elevationErrors));
+fprintf('The largest radius error is: %f \n',max(radiusErrors));
