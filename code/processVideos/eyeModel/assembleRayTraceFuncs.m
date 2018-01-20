ca7fa3d10b7de7e2d40e7be5d3b537ffd2220d6b
gkaguirre <gkaguirre@me.com>
2018-01-20 17:01:24 -0500

    A single, consolidated routine to make the ray tracing symbolic fxns
    Signed-off-by: gkaguirre <gkaguirre@me.com>

---
+++ code/processVideos/eyeModel/assembleRayTraceFuncs.m
@@ -0,0 +1,104 @@
+function [rayTraceFuncs] = assembleRayTraceFuncs( sceneGeometry )
+
+%% 2D ray tracing function through the cornea
+% Assumes the camera resides in air (index of refraction is 1.0)
+eye = sceneGeometry.eye;
+syms z h theta
+opticalSystem = [nan nan eye.aqueousRefractiveIndex; ...
+    eye.corneaBackSurfaceCenter(1) -eye.corneaBackSurfaceRadius eye.corneaRefractiveIndex; ...
+    eye.corneaFrontSurfaceCenter(1) -eye.corneaFrontSurfaceRadius 1.0];
+outputRay2D = rayTraceCenteredSphericalSurfaces([z h], theta, opticalSystem);
+rayTraceFuncs.cornea = {matlabFunction(outputRay2D)};
+
+
+%% Distance of the ray intersection from the camera node
+syms p1 p2 p3
+syms theta_p1p2 theta_p1p3
+outputRayEyeWorld2D_p1p2 = rayTraceFuncs.cornea{1}(p2,theta_p1p2,p1);
+outputRayEyeWorld2D_p1p3 = rayTraceFuncs.cornea{1}(p3,theta_p1p3,p1);
+
+% Add the third, constant dimension for the output rays
+outputRayEyeWorld_p1p2=[outputRayEyeWorld2D_p1p2(1,1) outputRayEyeWorld2D_p1p2(1,2) p3;...
+    outputRayEyeWorld2D_p1p2(2,1) outputRayEyeWorld2D_p1p2(2,2) p3];
+outputRayEyeWorld_p1p3=[outputRayEyeWorld2D_p1p3(1,1) p2 outputRayEyeWorld2D_p1p3(1,2);...
+    outputRayEyeWorld2D_p1p3(2,1) p2 outputRayEyeWorld2D_p1p3(2,2)];
+
+% prepare to rotate the outputRay into the sceneWorld coordinates
+syms cameraTranslationX cameraTranslationY cameraTranslationZ rotationCenterDepth
+syms eyeAzimuthRads eyeElevationRads
+eyeTorsionRads=0;
+
+R3 = [cos(eyeAzimuthRads) -sin(eyeAzimuthRads) 0; sin(eyeAzimuthRads) cos(eyeAzimuthRads) 0; 0 0 1];
+R2 = [cos(eyeElevationRads) 0 sin(eyeElevationRads); 0 1 0; -sin(eyeElevationRads) 0 cos(eyeElevationRads)];
+R1 = [1 0 0; 0 cos(eyeTorsionRads) -sin(eyeTorsionRads); 0 sin(eyeTorsionRads) cos(eyeTorsionRads)];
+
+eyeRotation = R1*R2*R3;
+
+% Shift the eyeWorld ray to the rotational center of the eye,
+% rotate for this eye pose, undo the centering
+outputRayHeadWorld_p1p2(1,:)=outputRayEyeWorld_p1p2(1,:)-rotationCenterDepth;
+outputRayHeadWorld_p1p2(2,:)=outputRayEyeWorld_p1p2(2,:)-rotationCenterDepth;
+outputRayHeadWorld_p1p2 = (eyeRotation*(outputRayHeadWorld_p1p2)')';
+outputRayHeadWorld_p1p2(1,:)=outputRayHeadWorld_p1p2(1,:)+rotationCenterDepth;
+outputRayHeadWorld_p1p2(2,:)=outputRayHeadWorld_p1p2(2,:)+rotationCenterDepth;
+
+outputRayHeadWorld_p1p3(1,:)=outputRayEyeWorld_p1p3(1,:)-rotationCenterDepth;
+outputRayHeadWorld_p1p3(2,:)=outputRayEyeWorld_p1p3(2,:)-rotationCenterDepth;
+outputRayHeadWorld_p1p3 = (eyeRotation*(outputRayHeadWorld_p1p3)')';
+outputRayHeadWorld_p1p3(1,:)=outputRayHeadWorld_p1p3(1,:)+rotationCenterDepth;
+outputRayHeadWorld_p1p3(2,:)=outputRayHeadWorld_p1p3(2,:)+rotationCenterDepth;
+
+% Re-arrange the head world coordinate frame to transform to the scene
+% world coordinate frame
+outputRaySceneWorld_p1p2 = outputRayHeadWorld_p1p2(:,[2 3 1]);
+outputRaySceneWorld_p1p3 = outputRayHeadWorld_p1p3(:,[2 3 1]);
+
+% We reverse the direction of the Y axis so that positive elevation of the
+% eye corresponds to a movement of the pupil upward in the image
+outputRaySceneWorld_p1p2(:,2) = outputRaySceneWorld_p1p2(:,2)*(-1);
+outputRaySceneWorld_p1p3(:,2) = outputRaySceneWorld_p1p3(:,2)*(-1);
+
+% Obtain an expression for X and Y distances between the nodal point of the camera in the sceneWorld plane and the
+% point at which the ray will strike the plane that contains the camera
+slope_xZ =(outputRaySceneWorld_p1p2(2,1)-outputRaySceneWorld_p1p2(1,1))/(outputRaySceneWorld_p1p2(2,3)-outputRaySceneWorld_p1p2(1,3));
+slope_yZ =(outputRaySceneWorld_p1p2(2,2)-outputRaySceneWorld_p1p2(1,2))/(outputRaySceneWorld_p1p2(2,3)-outputRaySceneWorld_p1p2(1,3));
+cameraPlaneX = outputRaySceneWorld_p1p2(1,1)+((cameraTranslationZ-outputRaySceneWorld_p1p2(1,3))*slope_xZ);
+cameraPlaneY = outputRaySceneWorld_p1p2(1,2)+((cameraTranslationZ-outputRaySceneWorld_p1p2(1,3))*slope_yZ);
+rayTraceFuncs.cameraNodeDistanceError_p1p2 = {matlabFunction(...
+    abs(cameraTranslationX-cameraPlaneX) + ...
+    abs(cameraTranslationY-cameraPlaneY) ...
+    )};
+
+slope_xZ =(outputRaySceneWorld_p1p3(2,1)-outputRaySceneWorld_p1p3(1,1))/(outputRaySceneWorld_p1p3(2,3)-outputRaySceneWorld_p1p3(1,3));
+slope_yZ =(outputRaySceneWorld_p1p3(2,2)-outputRaySceneWorld_p1p3(1,2))/(outputRaySceneWorld_p1p3(2,3)-outputRaySceneWorld_p1p3(1,3));
+cameraPlaneX = outputRaySceneWorld_p1p3(1,1)+((cameraTranslationZ-outputRaySceneWorld_p1p3(1,3))*slope_xZ);
+cameraPlaneY = outputRaySceneWorld_p1p3(1,2)+((cameraTranslationZ-outputRaySceneWorld_p1p3(1,3))*slope_yZ);
+rayTraceFuncs.cameraNodeDistanceError_p1p3 = {matlabFunction(...
+    abs(cameraTranslationX-cameraPlaneX) + ...
+    abs(cameraTranslationY-cameraPlaneY) ...
+    )};
+
+
+%% Assemble the virtual image ray
+outputRayEyeWorld_p1p2 = rayTraceFuncs.cornea{1}(p2, theta_p1p2, p1);
+outputRayEyeWorld_p1p3 = rayTraceFuncs.cornea{1}(p3, theta_p1p3, p1);
+
+% Adjust the p1 (optical axis) position of the rays to have the
+% their initial position at the same p1
+slope =(outputRayEyeWorld_p1p2(2,2)-outputRayEyeWorld_p1p2(1,2))/(outputRayEyeWorld_p1p2(2,1)-outputRayEyeWorld_p1p2(1,1));
+zOffset=outputRayEyeWorld_p1p2(1,1)-p1;
+outputRayEyeWorld_p1p2(:,1)=outputRayEyeWorld_p1p2(:,1)-zOffset;
+outputRayEyeWorld_p1p2(:,2)=outputRayEyeWorld_p1p2(:,2)-(zOffset*slope);
+
+slope =(outputRayEyeWorld_p1p3(2,2)-outputRayEyeWorld_p1p3(1,2))/(outputRayEyeWorld_p1p3(2,1)-outputRayEyeWorld_p1p3(1,1));
+zOffset=outputRayEyeWorld_p1p3(1,1)-p1;
+outputRayEyeWorld_p1p3(:,1)=outputRayEyeWorld_p1p3(:,1)-zOffset;
+outputRayEyeWorld_p1p3(:,2)=outputRayEyeWorld_p1p3(:,2)-(zOffset*slope);
+
+% Combine the two dimensions into a single, 3D ray
+outputRayEyeWorld(1,:) = [outputRayEyeWorld_p1p2(1,1) outputRayEyeWorld_p1p2(1,2) outputRayEyeWorld_p1p3(1,2)];
+outputRayEyeWorld(2,:) = [outputRayEyeWorld_p1p2(2,1) outputRayEyeWorld_p1p2(2,2) outputRayEyeWorld_p1p3(2,2)];
+
+rayTraceFuncs.virtualImageRay = {matlabFunction(outputRayEyeWorld)};
+
+end
