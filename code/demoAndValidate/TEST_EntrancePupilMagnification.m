7966e5342dedb79664832ba0bf5120aec8d48005
gkaguirre <gkaguirre@me.com>
2018-01-24 14:28:41 -0500

    Working on this
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/demoAndValidate/TEST_EntrancePupilMagnification.m
+++ code/demoAndValidate/TEST_EntrancePupilMagnification.m
@@ -24,14 +24,17 @@
 
 close all
 
-% Obtain the default sceneGeometry, although with no lens distortion, and
-% setting the camera distance to 100 mm.
+% Obtain the default sceneGeometry with the following modifications:
+% - no lens distortion
+% - camera distance to 100 mm
+% - center of rotation at the corneal apex
 
 initialCameraPosition = [0; 0; 100];
 
 sceneGeometry = estimateSceneGeometry([],[], ...
     'radialDistortionVector', [0 0], ...
-    'extrinsicTranslationVector',initialCameraPosition);
+    'extrinsicTranslationVector',initialCameraPosition,...
+    'rotationCenterDepth',0);
 
 % Assemble the ray tracing functions
 rayTraceFuncs = assembleRayTraceFuncs( sceneGeometry );
@@ -46,19 +49,22 @@ veridicalPupilPixelDiam = max(imagePoints(:,1)')-min(imagePoints(:,1)');
 % horizontal and vertical diameter of the pupil image
 horizDiam=[];
 vertDiam=[];
-for azimuthDeg = 0:10:80
+for azimuthDeg = 0:5:60
     eyeParams=[-azimuthDeg 0 0 pupilDiam/2];
     % We first identify the position of the corneal
     % apex in sceneWorld coordinates following the eye rotation
-    [~, ~, sceneWorldPoints, ~, pointLabels] = pupilProjection_fwd(eyeParams, sceneGeometry, rayTraceFuncs);
+    [~, ~, sceneWorldPoints, ~, pointLabels] = pupilProjection_fwd(eyeParams, sceneGeometry, rayTraceFuncs, 'fullEyeModelFlag', true);
     cornealApexIdx = find(strcmp(pointLabels,'cornealApex'));
+    pupilCenterIdx = find(strcmp(pointLabels,'pupilCenter'));
     % In the Fedtke model, and in the measurements reported by Jay and
-    % Spring & Stiles, the camera is positioned at a fixed distance from the corneal apex, and translates horizontally with the
-    % eye as it rotates. We therefore adjust the sceneGeometry so that the
+    % Spring & Stiles, the camera is positioned such that at translates horizontally to be
+    % centered on the center of the pupil.
     tmpSceneGeometry = sceneGeometry;
-    tmpSceneGeometry.extrinsicTranslationVector = initialCameraPosition + sceneWorldPoints(cornealApexIdx,:)';
+%    sceneWorldPoints(cornealApexIdx,1:3)
+%   tmpSceneGeometry.extrinsicTranslationVector(1) = initialCameraPosition(1) + sceneWorldPoints(pupilCenterIdx,1);
+%   tmpSceneGeometry.extrinsicTranslationVector(3) = initialCameraPosition(3) + sceneWorldPoints(pupilCenterIdx,3);
     % Find the image points for this translated camera position.
-    [~, imagePoints] = pupilProjection_fwd(eyeParams, sceneGeomettmpSceneGeometryry, rayTraceFuncs, 'nPupilPerimPoints',50);
+    [~, imagePoints] = pupilProjection_fwd(eyeParams, tmpSceneGeometry, rayTraceFuncs, 'nPupilPerimPoints',50);
     horizDiam =[horizDiam max(imagePoints(:,1)')-min(imagePoints(:,1)')];
     vertDiam  =[vertDiam max(imagePoints(:,2)')-min(imagePoints(:,2)')];
 end
@@ -70,27 +76,36 @@ vertMag = vertDiam ./ veridicalPupilPixelDiam;
 MtanFedtke = @(azimuthDeg, p) (1.133-6.3e-4*p^2)*cosd((-0.8798+4.8e-3*p).*azimuthDeg+3.7e-4.*azimuthDeg.^2);
 MsagFedtke = @(azimuthDeg) 4.4e-6.*(azimuthDeg.^2.299)+1.125;
 
+% figure
+% subplot(2,1,1);
+% plot(0:1:80,MtanFedtke(0:1:80, pupilDiam),'-r');
+% hold on
+% plot(0:1:80,cosd(0:1:80),'--k');
+% plot(0:10:80,horizMag,'xk');
+% xlim([0 90]);
+% ylim([0.4 1.2]);
+% 
+% subplot(2,1,2);
+% plot(0:1:80,MsagFedtke(0:1:80),'-r');
+% hold on
+% plot(0:10:80,vertMag,'xk');
+% xlim([0 90]);
+% ylim([1.1 1.25]);
+% 
+% figure
+% plot(0:1:80,MtanFedtke(0:1:80, pupilDiam)./MsagFedtke(0:1:80),'-r');
+% hold on
+% plot(0:10:80,horizDiam./vertDiam,'xk');
+% xlim([0 90]);
+% ylim([0.3 1.1]);
+
 figure
-subplot(2,1,1);
-plot(0:1:80,MtanFedtke(0:1:80, pupilDiam),'-r');
+mathurEq = @(azimuthdeg) 0.99.*cosd((azimuthdeg+5.3)/1.121);
+plot(-80:1:80,mathurEq(-80:1:80),'-r');
 hold on
-plot(0:1:80,cosd(0:1:80),'--k');
-plot(0:10:80,horizMag,'xk');
-xlim([0 90]);
-ylim([0.4 1.2]);
-
-subplot(2,1,2);
-plot(0:1:80,MsagFedtke(0:1:80),'-r');
-hold on
-plot(0:10:80,vertMag,'xk');
-xlim([0 90]);
-ylim([1.1 1.25]);
-
-figure
-plot(0:1:80,MtanFedtke(0:1:80, pupilDiam)./MsagFedtke(0:1:80),'-r');
-hold on
-plot(0:10:80,horizDiam./vertDiam,'xk');
-xlim([0 90]);
-ylim([0.3 1.1]);
-
+plot(-80:1:80,cosd(-80:1:80),'--k');
+plot(0:-5:-60,horizDiam./vertDiam ,'xk');
+plot(0:5:60,horizDiam./vertDiam ,'xk');
+xlim([-90 90]);
+ylim([0 1.1]);
 
