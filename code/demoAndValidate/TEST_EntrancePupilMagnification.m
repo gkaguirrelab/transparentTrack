99b711e3d403cd30cc667cfd25d3e1344fd374e9
gkaguirre <gkaguirre@me.com>
2018-01-24 15:42:41 -0500

    Developing
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/demoAndValidate/TEST_EntrancePupilMagnification.m
+++ code/demoAndValidate/TEST_EntrancePupilMagnification.m
@@ -18,6 +18,13 @@
 %   angle, and compares the output of their model to empirical measurements
 %   made by Jay (1962) and Spring & Stiles (1948).
 %
+%   In Mathur 2013, empirical measurements of the ratio of pupil width to
+%   height were made as a function of viewing angle:
+%
+%       Mathur, Ankit, Julia Gehrmann, and David A. Atchison. "Pupil shape
+%       as viewed along the horizontal visual field." Journal of vision
+%       13.6 (2013): 3-3.
+%
 %   Here we calculates these values for our model, and compare the results
 %   to the prior studies.
 %
@@ -28,12 +35,13 @@ close all
 % - no lens distortion
 % - camera distance to 100 mm
 % - center of rotation at the corneal apex
-
-initialCameraPosition = [0; 0; 100];
-
+%
+% By having the eye rotate around the corneal apex, we match the conditions
+% of these prior papers in which the camera maintained at a constant
+% distance from the corneal apex.
 sceneGeometry = estimateSceneGeometry([],[], ...
     'radialDistortionVector', [0 0], ...
-    'extrinsicTranslationVector',initialCameraPosition,...
+    'extrinsicTranslationVector',[0; 0; 100],...
     'rotationCenterDepth',0);
 
 % Assemble the ray tracing functions
@@ -51,20 +59,7 @@ horizDiam=[];
 vertDiam=[];
 for azimuthDeg = 0:5:60
     eyeParams=[-azimuthDeg 0 0 pupilDiam/2];
-    % We first identify the position of the corneal
-    % apex in sceneWorld coordinates following the eye rotation
-    [~, ~, sceneWorldPoints, ~, pointLabels] = pupilProjection_fwd(eyeParams, sceneGeometry, rayTraceFuncs, 'fullEyeModelFlag', true);
-    cornealApexIdx = find(strcmp(pointLabels,'cornealApex'));
-    pupilCenterIdx = find(strcmp(pointLabels,'pupilCenter'));
-    % In the Fedtke model, and in the measurements reported by Jay and
-    % Spring & Stiles, the camera is positioned such that at translates horizontally to be
-    % centered on the center of the pupil.
-    tmpSceneGeometry = sceneGeometry;
-%    sceneWorldPoints(cornealApexIdx,1:3)
-%   tmpSceneGeometry.extrinsicTranslationVector(1) = initialCameraPosition(1) + sceneWorldPoints(pupilCenterIdx,1);
-%   tmpSceneGeometry.extrinsicTranslationVector(3) = initialCameraPosition(3) + sceneWorldPoints(pupilCenterIdx,3);
-    % Find the image points for this translated camera position.
-    [~, imagePoints] = pupilProjection_fwd(eyeParams, tmpSceneGeometry, rayTraceFuncs, 'nPupilPerimPoints',50);
+    [~, imagePoints] = pupilProjection_fwd(eyeParams, sceneGeometry, rayTraceFuncs, 'nPupilPerimPoints',50);
     horizDiam =[horizDiam max(imagePoints(:,1)')-min(imagePoints(:,1)')];
     vertDiam  =[vertDiam max(imagePoints(:,2)')-min(imagePoints(:,2)')];
 end
@@ -76,36 +71,62 @@ vertMag = vertDiam ./ veridicalPupilPixelDiam;
 MtanFedtke = @(azimuthDeg, p) (1.133-6.3e-4*p^2)*cosd((-0.8798+4.8e-3*p).*azimuthDeg+3.7e-4.*azimuthDeg.^2);
 MsagFedtke = @(azimuthDeg) 4.4e-6.*(azimuthDeg.^2.299)+1.125;
 
-% figure
-% subplot(2,1,1);
-% plot(0:1:80,MtanFedtke(0:1:80, pupilDiam),'-r');
-% hold on
-% plot(0:1:80,cosd(0:1:80),'--k');
-% plot(0:10:80,horizMag,'xk');
-% xlim([0 90]);
-% ylim([0.4 1.2]);
-% 
-% subplot(2,1,2);
-% plot(0:1:80,MsagFedtke(0:1:80),'-r');
-% hold on
-% plot(0:10:80,vertMag,'xk');
-% xlim([0 90]);
-% ylim([1.1 1.25]);
-% 
-% figure
-% plot(0:1:80,MtanFedtke(0:1:80, pupilDiam)./MsagFedtke(0:1:80),'-r');
-% hold on
-% plot(0:10:80,horizDiam./vertDiam,'xk');
-% xlim([0 90]);
-% ylim([0.3 1.1]);
+figure
+subplot(2,1,1);
+plot(0:1:80,MtanFedtke(0:1:80, pupilDiam),'-r');
+hold on
+plot(0:1:80,cosd(0:1:80),'--k');
+plot(0:5:60,horizMag,'xk');
+xlim([0 90]);
+ylim([0.4 1.2]);
+
+subplot(2,1,2);
+plot(0:1:80,MsagFedtke(0:1:80),'-r');
+hold on
+plot(0:5:60,vertMag,'xk');
+xlim([0 90]);
+ylim([1.1 1.25]);
 
 figure
+plot(0:1:80,MtanFedtke(0:1:80, pupilDiam)./MsagFedtke(0:1:80),'-r');
+hold on
+plot(0:5:60,horizDiam./vertDiam,'xk');
+xlim([0 90]);
+ylim([0.3 1.1]);
+
+% Search over pupil center locations to find the best match to the Marthur
+% equation
+azimuthsDeg = -60:10:60;
 mathurEq = @(azimuthdeg) 0.99.*cosd((azimuthdeg+5.3)/1.121);
+myObjFunc = @(x) sum((mathurEq(azimuthsDeg) - localCompute(x,azimuthsDeg,pupilDiam,sceneGeometry,rayTraceFuncs)).^2);
+
+x0=[0.5 -0.25];
+[x, fVal]=fmincon(myObjFunc,x0,[],[],[],[],[-1 -1],[1 1]);
+
+diamRatio = localCompute(x,azimuthsDeg,pupilDiam,sceneGeometry,rayTraceFuncs);
+
+% This is Figure 10 of Mathur 2013
+figure
 plot(-80:1:80,mathurEq(-80:1:80),'-r');
 hold on
 plot(-80:1:80,cosd(-80:1:80),'--k');
-plot(0:-5:-60,horizDiam./vertDiam ,'xk');
-plot(0:5:60,horizDiam./vertDiam ,'xk');
+plot(azimuthsDeg,diamRatio ,'xk');
 xlim([-90 90]);
 ylim([0 1.1]);
+
+
+%% LOCAL FUNCTION
+function diamRatio = localCompute(x,azimuthsDeg,pupilDiam,sceneGeometry,rayTraceFuncs)
+horizDiam=[];
+vertDiam=[];
+sceneGeometry.eye.pupilCenter = [-3.7,x(1),x(2)];
+for ii = 1:length(azimuthsDeg)
+    eyeParams=[-azimuthsDeg(ii) 0 0 pupilDiam/2];
+    [~, imagePoints] = pupilProjection_fwd(eyeParams, sceneGeometry, rayTraceFuncs, 'nPupilPerimPoints',50);
+    horizDiam =[horizDiam max(imagePoints(:,1)')-min(imagePoints(:,1)')];
+    vertDiam  =[vertDiam max(imagePoints(:,2)')-min(imagePoints(:,2)')];
+end
+diamRatio=horizDiam./vertDiam;
+end
+
 
