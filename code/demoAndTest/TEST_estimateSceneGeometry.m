4c7c5e74cdcffd38106712dea45249a922b1e885
gkaguirre <gkaguirre@me.com>
2018-01-28 17:54:15 -0500

    Basic test
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/demoAndTest/TEST_estimateSceneGeometry.m
+++ code/demoAndTest/TEST_estimateSceneGeometry.m
@@ -8,30 +8,38 @@
 close all
 
 % Obtain the default sceneGeometry
-% - no lens distortion
-% - camera distance to 100 mm
-% - center of rotation at the corneal apex
-%
-% By having the eye rotate around the corneal apex, we match the conditions
-% of these prior papers in which the camera was maintained at a constant
-% distance from the corneal apex.
-veridicalSceneGeometry = estimateSceneGeometry([],[],'eyeLaterality','Right');
+defaultSceneGeometry = estimateSceneGeometry([],[],'eyeLaterality','Right');
 
-% Assemble the ray tracing functions
-rayTraceFuncs = assembleRayTraceFuncs( veridicalSceneGeometry );
-
-% Create a set of ellipses from the veridial geometry
-idx=1;
-for azi=-15:15:15
-    for ele=-15:15:15
-        pupilRadius = 2*randn()/2;
-        eyeParams=[azi, ele, 0, pupilRadius];
-        pupilData.initial.ellipses.values(idx,:) = pupilProjection_fwd(eyeParams, veridicalSceneGeometry, rayTraceFuncs);
-        pupilData.initial.ellipses.RMSE(idx) = 1;
-        idx=idx+1;
+% Create variations in center of rotation and camera distance
+vsg={};
+esg={};
+resultIdx = 1;
+for CoR = -14:0.25:-13
+    for cameraDistance = 125:25:175
+        veridicalSceneGeometry = defaultSceneGeometry;
+        veridicalSceneGeometry.eye.rotationCenter(1) = CoR;
+        veridicalSceneGeometry.extrinsicTranslationVector(3) = cameraDistance;
+        
+        % Assemble the ray tracing functions
+        rayTraceFuncs = assembleRayTraceFuncs( veridicalSceneGeometry );
+        
+        % Create a set of ellipses from the veridial geometry
+        ellipseIdx=1;
+        for azi=-15:15:15
+            for ele=-15:15:15
+                pupilRadius = 2*randn()/2;
+                eyeParams=[azi, ele, 0, pupilRadius];
+                pupilData.initial.ellipses.values(ellipseIdx,:) = pupilProjection_fwd(eyeParams, veridicalSceneGeometry, rayTraceFuncs);
+                pupilData.initial.ellipses.RMSE(ellipseIdx) = 1;
+                ellipseIdx=ellipseIdx+1;
+            end
+        end
+        
+        estimatedSceneGeometry = estimateSceneGeometry(pupilData,'','useParallel',true,'ellipseArrayList',1:1:ellipseIdx-1);
+        
+        vsg{resultIdx}=veridicalSceneGeometry;
+        esg{resultIdx}=estimatedSceneGeometry;
+        resultIdx = resultIdx+1;
     end
 end
-
-estimatedSceneGeometry = estimateSceneGeometry(pupilData,'','useParallel',true,'ellipseArrayList',1:1:idx-1);
-
 
