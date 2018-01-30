ac8427121c827d971a6866be9eb0b71d93f9b9cd
gkaguirre <gkaguirre@me.com>
2018-01-29 19:31:53 -0500

    Renamed, plus several change
    - Bug fix to adjustment of rotation center for ametropia
    - Switched to using the parameter parser
    - Added an optional key/value to specify total axial length, when set, this value is used to scale the size of the posterior chamber, while maintaining the specified ametropia.
    
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/processVideos/eyeModel/eyeModelParameters.m
+++ code/processVideos/eyeModel/eyeModelParameters.m
@@ -1,4 +1,4 @@
-function eye = modelEyeParameters( spectacleRefractionDiopters, eyeLaterality, species )
+function eye = eyeModelParameters( varargin )
 % Return the parameters of a model eye
 %
 % Syntax:
@@ -23,7 +23,10 @@ function eye = modelEyeParameters( spectacleRefractionDiopters, eyeLaterality, s
 %   values of depth towards the center of the eye.
 %
 % Inputs:
-%   spectacleRefractionDiopters - Scalar, in units of diopters. The
+%   none
+%
+% Optional key/value pairs:
+%  'spectacleRefractionDiopters' - Scalar, in units of diopters. The
 %                           dimensions of the posterior chamber of the eye
 %                           (and to a lesser extent the curvature of the
 %                           cornea) change with the observed refractive
@@ -31,10 +34,18 @@ function eye = modelEyeParameters( spectacleRefractionDiopters, eyeLaterality, s
 %                           spherical refractive correction for the
 %                           subject. A negative number is the correction
 %                           that would be used for a myopic person.
-%   eyeLaterality         - A text string that specifies which eye (left,
+%  'axialLength'          - When set, this forces the axial length of the 
+%                           eye to this value in millimeters. As the
+%                           modeled anterior chamber depth is not variable,
+%                           this change is enforced on the posterior
+%                           chamber. The remaining dimensions of the
+%                           posterior chamber are scaled to fit the
+%                           proportions predicted by the Atchison model for
+%                           the specified ametropia.
+%  'eyeLaterality'        - A text string that specifies which eye (left,
 %                           right) to model. Allowed values (in any case)
 %                           are {'left','right','L','R','OS','OD'}
-%   species               - A text string that specifies the species to be
+%  'species'              - A text string that specifies the species to be
 %                           modeled. Supported values (in any case) are
 %                           {'human'}
 %                       
@@ -50,34 +61,31 @@ function eye = modelEyeParameters( spectacleRefractionDiopters, eyeLaterality, s
 %}
 %{
     % Parameters for an myopic (-3), left, human eye
-    eye = modelEyeParameters(-3,'left');
+    eye = modelEyeParameters('spectacleRefractionDiopters',-3,'eyeLaterality','left');
 %}
 
-%% Check the input
-if nargin==0
-    spectacleRefractionDiopters=0;
-    eyeLaterality = 'right';
-    species = 'human';
-end
 
-if nargin==1
-    eyeLaterality = 'right';
-    species = 'human';
-end
+%% input parser
+p = inputParser; p.KeepUnmatched = true;
 
-if nargin==2
-    species = 'human';
-end
+% Optional
+p.addParameter('spectacleRefractionDiopters',0,@isnumeric);
+p.addParameter('axialLength',[],@(x)(isempty(x) || isnumeric(x)));
+p.addParameter('eyeLaterality','Right',@ischar);
+p.addParameter('species','Human',@ischar);
+
+% parse
+p.parse(varargin{:})
 
 % Switch parameters at the top level by species
-switch species
+switch p.Results.species
     case {'human','Human','HUMAN'}
 
         % These values taken from Achison 2006, Table 1. The center of the
         % cornea circle for the back surface is positioned so that there is
         % 0.55 between the front and back surface of the cornea at the
         % apex.
-        eye.corneaFrontSurfaceRadius = 7.77 + 0.022*spectacleRefractionDiopters;
+        eye.corneaFrontSurfaceRadius = 7.77;
         eye.corneaFrontSurfaceCenter = [-7.77 0 0];
         eye.corneaBackSurfaceRadius = 6.4;
         eye.corneaBackSurfaceCenter = [-7.22 0 0];
@@ -99,7 +107,7 @@ switch species
         % Bennett, Edward S., and Barry A. Weissman, eds. Clinical contact
         % lens practice. Lippincott Williams & Wilkins, 2005, p119
         %
-        switch eyeLaterality
+        switch p.Results.eyeLaterality
             case {'right','RIGHT','Right','R','r','od','OD'}
                 eye.pupilCenter = [-3.7 0.1445 0.3639];
             case {'left','LEFT','Left','L','l','os','OS'}
@@ -108,7 +116,8 @@ switch species
                 error('Please specify a valid eye laterality for the model eye');
         end
         
-        % Define the properties of the iris
+        % Define the properties of the iris. Geoff needs to do some work to
+        % find and justify values here.
         eye.irisRadius = 5;
         eye.irisCenter = [-4.5 0 0];
         
@@ -116,11 +125,23 @@ switch species
         % dimensions of the ellipsoid are set by the values observed for
         % the radius of curvature of the retina in Atchison 2005. Note the
         % greater dependence of the first dimension (axial length) upon
-        % refractive error.
+        % refractive error.        
         eye.posteriorChamberRadii = [...
-            10.148 - 0.163 * spectacleRefractionDiopters ...
-            11.365 - 0.090 * spectacleRefractionDiopters ...
-            11.455 - 0.043 * spectacleRefractionDiopters ];
+            10.148 - 0.163 * p.Results.spectacleRefractionDiopters ...
+            11.365 - 0.090 * p.Results.spectacleRefractionDiopters ...
+            11.455 - 0.043 * p.Results.spectacleRefractionDiopters ];
+        
+        if ~isempty(p.Results.axialLength)
+            % If a specific axial length is passed (perhaps obtained by
+            % measurement using the IOL Master device), set the model eye
+            % to have this length, and scale the other dimensions of the
+            % posterior chamber to maintain the specified ametropia.
+            % We adjust the axial length for the component of the anterior
+            % chamber that contibutes to length, which is calculated below
+            % to be 3.2964.
+            scaleFactor = (p.Results.axialLength - 3.2964) / (eye.posteriorChamberRadii(1)*2);
+            eye.posteriorChamberRadii = eye.posteriorChamberRadii .* scaleFactor;
+        end
         
         % We position the posterior chamber by placing the anterior apex of
         % the ellipsoid at a fixed distance behind the corneal apex. The
@@ -146,8 +167,21 @@ switch species
         % Strabismus" American Orthoptic Journal 51.1 (2001): 161-162.
         % Spectacle refraction adjusts the axial length of the eye. We
         % assume here that the center of rotation reflects this change in
-        % length.
-        eye.rotationCenter = [-13.3+(0.022*spectacleRefractionDiopters)/2 0 0];
+        % length. Specifically, we assume that the increase in the radius
+        % of curvature of the posterior chamber described by the Atchison
+        % model results in an equivalent lengthening of the radius of
+        % rotation. Support for this 1:1 relationship is found in the paper
+        %
+        %   Dick, Graham L., Bryan T. Smith, and Peter L. Spanos. 
+        %   "Axial length and radius of rotation of the eye." 
+        %   Clinical and Experimental Optometry 73.2 (1990): 43-50.
+        %
+        % Specifically, Figure 6 shows that, for each mm of increase in 
+        % the axial length of an eye, the center of rotation tended to
+        % increase by 0.5 mm. Thus, there is a 1:1 relationship of axial
+        % radius and rotation length.
+        %
+        eye.rotationCenter = [-13.3 - (0.163 * spectacleRefractionDiopters) 0 0];
         
         % Refractive index values from Atchison 2006.
         eye.corneaRefractiveIndex = 1.376;
