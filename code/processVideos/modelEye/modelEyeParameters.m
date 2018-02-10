9e7178ea8b99a077a5f6b4bbfa5fbb402d9b105e
gkaguirre <gkaguirre@me.com>
2018-02-10 10:11:58 -0500

    Added calculation of kappa parameter
    Also, set the coordinate space aligned with the pupil center. Consequently, the iris center is now slightly displaced.
    
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/processVideos/modelEye/modelEyeParameters.m
+++ code/processVideos/modelEye/modelEyeParameters.m
@@ -18,9 +18,11 @@ function eye = modelEyeParameters( varargin )
 %   Atchison uses the dimensions [x, y, z] corresponding to the width,
 %   height, and depth (axial length) of the model eye. The parameters
 %   returned by this routine correspond to the eyeWorld coordinate space
-%   used in pupilProjection_fwd, which is relative to the apex of the
-%   cornea and has the dimensions [depth, horizontal, vertical]; negative
-%   values of depth towards the center of the eye.
+%   used in pupilProjection_fwd, which is relative to the pupil axis, with
+%   the apex of the cornea set as zero in depth. The space has the
+%   dimensions [depth, horizontal, vertical]; negative values of depth
+%   towards the center of the eye. The model assumes the optical and pupil
+%   axis of the eye are algined.
 %
 % Inputs:
 %   none
@@ -34,14 +36,18 @@ function eye = modelEyeParameters( varargin )
 %                           spherical refractive correction for the
 %                           subject. A negative number is the correction
 %                           that would be used for a myopic person.
-%  'axialLength'          - When set, this fixes the axial length of the 
-%                           eye to the passed value in millimeters. As the
-%                           modeled anterior chamber depth is not variable,
-%                           this change is enforced on the posterior
-%                           chamber. The remaining dimensions of the
-%                           posterior chamber are scaled to fit the
+%  'axialLength'          - Scalar. When set, this fixes the axial length 
+%                           of the eye to the passed value in millimeters.
+%                           As the modeled anterior chamber depth is not
+%                           variable, this change is enforced on the
+%                           posterior chamber. The remaining dimensions of
+%                           the posterior chamber are scaled to fit the
 %                           proportions predicted by the Atchison model for
 %                           the specified degree of ametropia.
+%  'kappaAngle'           - 1x2 matrix. This is the angle of the visual
+%                           in degrees w.r.t. to pupil axis. The values are
+%                           [azimuth, elevation].
+%                           the 
 %  'eyeLaterality'        - A text string that specifies which eye (left,
 %                           right) to model. Allowed values (in any case)
 %                           are {'left','right','L','R','OS','OD'}
@@ -71,6 +77,7 @@ p = inputParser; p.KeepUnmatched = true;
 % Optional
 p.addParameter('spectacleRefractionDiopters',0,@isnumeric);
 p.addParameter('axialLength',[],@(x)(isempty(x) || isnumeric(x)));
+p.addParameter('kappaAngle',[],@(x)(isempty(x) || isnumeric(x)));
 p.addParameter('eyeLaterality','Right',@ischar);
 p.addParameter('species','Human',@ischar);
 
@@ -91,13 +98,18 @@ switch p.Results.species
         eye.corneaBackSurfaceCenter = [-7.22 0 0];
         
         % We position the pupil plane at the depth of the anterior point of
-        % the lens. We have calculated for the optical model of the eye the
-        % decentration values that produce an entrance pupil width / height
-        % ratio that best matches empirical measurements (see
-        % TEST_entrancePupilShape). The values from that search are used
-        % here for the p2 and p3 position of the center of the pupil. In
-        % both eyes, the pupil center is shifted slightly nasally and
-        % downward. This is in keeping with empirical observations that:
+        % the lens. The coordinate space of the model eye is define with
+        % respect to the center of the pupil, so the p2 and p3 values are
+        % zero
+        eye.pupilCenter = [-3.7 0 0];
+        
+        % Define the properties of the iris. Geoff needs to do some work to
+        % find and justify values here.
+        % https://www.clspectrum.com/issues/2002/april-2002/contact-lens-case-reports        
+        eye.irisRadius = 5;
+                
+        % In both eyes, the iris center is shifted slightly temporally and
+        % upward with respect to the pupil center:
         %
         %   ...the typical entrance pupil is decentered
         %   approximately 0.15 mm nasally and 0.1 mm inferior to the
@@ -105,72 +117,75 @@ switch p.Results.species
         %
         % Bennett, Edward S., and Barry A. Weissman, eds. Clinical contact
         % lens practice. Lippincott Williams & Wilkins, 2005, p119
-        %
         switch p.Results.eyeLaterality
             case {'right','RIGHT','Right','R','r','od','OD'}
-                eye.pupilCenter = [-3.7 0.15 0.1];
+                eye.irisCenter = [-4.5 -0.15 -0.1];
             case {'left','LEFT','Left','L','l','os','OS'}
-                eye.pupilCenter = [-3.7 -0.15 0.1];
+                eye.irisCenter = [-4.5 0.15 -0.1];
             otherwise
                 error('Please specify a valid eye laterality for the model eye');
         end
         
-        % Define the properties of the iris. Geoff needs to do some work to
-        % find and justify values here.
-        % https://www.clspectrum.com/issues/2002/april-2002/contact-lens-case-reports        
-        eye.irisRadius = 5;
-        eye.irisCenter = [-4.5 0 0];
-        
         % The posterior chamber of the eye is modeled as an ellipsoid. The
         % dimensions of the ellipsoid are set by the values observed for
         % the radius of curvature of the retina in Atchison 2005. Note the
-        % greater dependence of the first dimension (axial length) upon
+        % greater dependence of the first (axial) dimension upon
         % refractive error.        
         eye.posteriorChamberRadii = [...
             10.148 - 0.163 * p.Results.spectacleRefractionDiopters ...
             11.365 - 0.090 * p.Results.spectacleRefractionDiopters ...
             11.455 - 0.043 * p.Results.spectacleRefractionDiopters ];
         
-        if ~isempty(p.Results.axialLength)
-            % If a specific axial length is passed (perhaps obtained by
-            % measurement using the IOL Master apparatus), set the model
-            % eye to have this length, and scale the other dimensions of
-            % the posterior chamber to maintain the specified ametropia. We
-            % adjust the axial length for the component of the anterior
-            % chamber that contibutes to length, which is calculated below
-            % to be 3.2964.
-            scaleFactor = (p.Results.axialLength - 3.2964) / (eye.posteriorChamberRadii(1)*2);
-            eye.posteriorChamberRadii = eye.posteriorChamberRadii .* scaleFactor;
-        end
-        
-        % We position the posterior chamber by placing the anterior apex of
-        % the ellipsoid at a fixed distance behind the corneal apex. The
-        % value for this fixed distance is derived from the Atchison model
-        % eye as follows: The total interior depth of the eye from Table 1
-        % for an emmetrope is:
+        % Our model holds the depth of the anterior chamber constant. To
+        % position the posterior chamber, we need to know the distance
+        % between the apex of the anterior chamber and the apex of the
+        % posterior chamber. The value for this fixed distance is derived
+        % from the Atchison 2006 model eye. The total interior depth of the
+        % eye from Table 1 for an emmetrope is:
         %
-        %   0.55 + 3.15 + 1.44 + 2.16 + 16.28 = 23.5800
+        %   0.55 + 3.15 + 1.44 + 2.16 + 16.28 = 23.58
         %
         % The axial length of the posterior chamber ellipsoid in an
         % emmetrope is:
         %
         %   10.1418 * 2 = 20.2836
         %
-        % Therefore, the apex of the posterior ellipsoid is 23.5800 -
-        % 20.2836 = 3.2964 mm behind the corneal apex.
-        eye.posteriorChamberCenter = ...
-            [(-3.2964 - eye.posteriorChamberRadii(1)) 0 0];
+        % Therefore, the apex of the posterior ellipsoid is:
+        %   23.5800 - 20.2836 = 3.2964 mm
+        % behind the corneal apex.
+        posteriorChamberApexDepth = 3.2964;
         
-        % The eye center of rotation is on average 13.3 mm behind the
-        % corneal apex per Gunter K. vonNoorden, MD; Emilio C. Campos
-        % "Binocular Vision and Ocular Motility Theory and Management of
-        % Strabismus" American Orthoptic Journal 51.1 (2001): 161-162.
-        % Spectacle refraction adjusts the axial length of the eye. We
-        % assume here that the center of rotation reflects this change in
-        % length. Specifically, we assume that the increase in the radius
-        % of curvature of the posterior chamber described by the Atchison
-        % model results in an equivalent lengthening of the radius of
-        % rotation. Support for this 1:1 relationship is found in the paper
+        % Compure and store axial length
+        if isempty(p.Results.axialLength)
+            eye.axialLength = posteriorChamberApexDepth + eye.posteriorChamberRadii(1)*2;
+        else
+            % If a specific axial length was passed (perhaps obtained by
+            % measurement using the IOL Master apparatus), set the model
+            % eye to have this length, and scale the other dimensions of
+            % the posterior chamber to maintain the specified ametropia. We
+            % adjust the axial length for the component of the anterior
+            % chamber that contibutes to length, which is calculated below
+            % to be 3.2964.
+            scaleFactor = (p.Results.axialLength - posteriorChamberApexDepth) / (eye.posteriorChamberRadii(1)*2);
+            eye.posteriorChamberRadii = eye.posteriorChamberRadii .* scaleFactor;
+            eye.axialLength = p.Results.axialLength;
+        end
+        
+        % Set the depth of the center of the posterior chamber
+        eye.posteriorChamberCenter = ...
+            [(-posteriorChamberApexDepth - eye.posteriorChamberRadii(1)) 0 0];
+        
+        % The eye center of rotation in emmetropes is on average 13.3 mm
+        % behind the corneal apex per Gunter K. vonNoorden, MD; Emilio C.
+        % Campos "Binocular Vision and Ocular Motility Theory and
+        % Management of Strabismus" American Orthoptic Journal 51.1 (2001):
+        % 161-162. Spectacle refraction adjusts the axial length of the
+        % eye. We assume here that the center of rotation reflects this
+        % change in length. Specifically, we assume that the increase in
+        % the radius of curvature of the posterior chamber described by the
+        % Atchison model produces an equivalent lengthening of the radius
+        % of rotation. Support for this 1:1 relationship is found in the
+        % paper:
         %
         %   Dick, Graham L., Bryan T. Smith, and Peter L. Spanos. 
         %   "Axial length and radius of rotation of the eye." 
@@ -180,19 +195,73 @@ switch p.Results.species
         % the axial length of an eye, the center of rotation tended to
         % increase by 0.5 mm. Thus, there is a 1:1 relationship of axial
         % radius and rotation length.
-        eye.rotationCenter = [(-3.152 - eye.posteriorChamberRadii(1)) 0 0];
+        % 
+        % In an emmetropic eye, the distance from the corneal apex to the
+        % center of the posterior chamber is
+        %
+        %   posteriorChamberApexDepth + posteriorChamberRadii(1), or
+        %   3.2964 + 10.148 = 13.4444
+        %
+        % This implies that the center of rotation of an emmetropic eye
+        % lies (13.4444 - 13.3 = 0.1444 mm) closer to the corneal surface
+        % than the position of the center of the posterior chamber.
+        eye.rotationCenter = [eye.posteriorChamberCenter(1)+0.1444 0 0];
         
         % Refractive index values from Atchison 2006.
         eye.corneaRefractiveIndex = 1.376;
         eye.aqueousRefractiveIndex = 1.3374;
         
-        % Calculate and store the axial length, and use the axial length
-        % to calculate kappa, the angle (in degrees) between the optical
-        % and visual axes of the eye. Positive values of kappa displaces
-        % the visual axis towards the nasal field of the eye
-        % Tabernero, Juan, et al. "Mechanism of compensation of aberrations in the human eye." JOSA A 24.10 (2007): 3274-3283.
-        
-        
+        % We now calculate kappa, which is the angle (in degrees) between
+        % the pupil and visual axes of the eye. The visual axis is
+        % displaced nasally and superiorly within the visual field relative
+        % to the pupil axis. We will define positive kappa values to be
+        % displacement nasally and upward This angle has been found to
+        % depend upon axial length:
+        %
+        %   Tabernero, Juan, et al. "Mechanism of compensation of
+        %   aberrations in the human eye." JOSA A 24.10 (2007): 3274-3283.
+        %
+        % Taberno and colleagues find a mean horizontal kappa of 5 degrees
+        % in emmetropes.
+        %
+        % The mean kappa value in emmetropes has been reported in several
+        % places:
+        %
+        %   Hashemi, Hassan, et al. "Distribution of angle kappa
+        %   measurements with Orbscan II in a population-based survey."
+        %   Journal of Refractive Surgery 26.12 (2010): 966-971.
+        %
+        %   Gharaee, Hamid, et al. "Angle kappa measurements: normal values
+        %   in healthy iranian population obtained with the Orbscan II."
+        %   Iranian Red Crescent Medical Journal 17.1 (2015).
+        %
+        % The Hashemi and Gharaee papers measure a total kappa angle that
+        % includes both horizontal and vertical components. Hashemi 2010
+        % provides the "kappa intercept angle" of the kappa vector distance
+        % with the pupil center. Oddly, the reported interecept angle
+        % varies substantially between eyes. Gharee 2015 notes that the
+        % great majority of studied eyes had their visual axis displaced
+        % superiorly w.r.t. the pupil axis. We take the mean intercept
+        % angle from the right eye of Hashemi 2010, which implies a 12
+        % degree angle with respect to the horizontal meridian. We assume a
+        % mean horizontal kappa of 5 degrees. We then calculate, based upon
+        % the Hashemi intercept angle, a vertical kappa of 1.0628 degrees.
+        %
+        % Tabernero 2007 Equation 6 expresses kappa (technically alpha, the
+        % angle w.r.t. the optical axis) as a function of axial length.
+        % Their formula assumes an emmetropic model eye of 24 mm, while the
+        % model eye used here has an emmetropic axial length of 23.592. The
+        % equation implemented below is adjusted so that an emmetropic eye
+        % of 23.5924 mm has a horizontal kappa = 5 degrees and a vertical
+        % kappa of 1.0628 degrees.
+        %
+        if isempty(p.Results.kappaAngle)
+            eye.kappaAngle(1) = atand((15.0924/(eye.axialLength-8.5000))*tand(5));
+            eye.kappaAngle(2) = atand((15.0924/(eye.axialLength-8.5000))*tand(1.0628));
+        else
+            eye.kappaAngle = p.Results.kappaAngle;
+        end
+            
     otherwise
         error('Please specify a valid species for the eye model');
 end
@@ -205,6 +274,7 @@ eye.meta.species = p.Results.species;
 eye.meta.units = 'mm';
 eye.meta.coordinates = 'eyeWorld';
 eye.meta.dimensions = {'depth (axial)' 'horizontal' 'vertical'};
+eye.meta.kappa = 'Degrees angle of visual axis w.r.t. pupil axis. Positive values nasal.';
 
 end % function
 
