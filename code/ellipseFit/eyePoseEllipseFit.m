f16e1f0135478dbea7dfd8d0ec4cbd33aa91552b
gkaguirre <gkaguirre@me.com>
2018-03-23 21:21:04 -0400

    Fixing the constraints
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/ellipseFit/eyePoseEllipseFit.m
+++ code/ellipseFit/eyePoseEllipseFit.m
@@ -74,14 +74,14 @@ meanYp = mean(Yp);
 % Set the bounds on the eyePose based upon the quadrant of the ellipse
 % center. We provide half a degree of wiggle in the fit around zero.
 if meanXp < CoP(1)
-    eyePoseUB(1) = .5;
+    eyePoseUB(1) = 5;
 else
-    eyePoseLB(1) = -.5;
+    eyePoseLB(1) = -5;
 end
 if meanYp > CoP(2)
-    eyePoseUB(2) = .5;
+    eyePoseUB(2) = 5;
 else
-    eyePoseLB(2) = -.5;
+    eyePoseLB(2) = -5;
 end
 
 % If x0 is undefined, we make a guess based upon the location of the center
