48dd0e7d909412ffe7e4e37f3f3091408a3ca417
gkaguirre <gkaguirre@me.com>
2018-03-27 10:48:39 -0400

    Utility function to load sceneGeometry and set up ray tracing func
    Signed-off-by: gkaguirre <gkaguirre@me.com>

---
+++ code/utilityFunctions/loadSceneGeometry.m
@@ -0,0 +1,60 @@
+function sceneGeometry = loadSceneGeometry(sceneGeometryFileName, verbosity)
+% Load sceneGeometry and instantiate the ray tracing function
+%
+% Syntax:
+%  sceneGeometry = loadSceneGeometry(sceneGeometryFileName, verbosity)
+%
+% Description:
+%   Loads the sceneGeometry file from file and ensures that a valid
+%   virtual image ray-tracing function is available.
+%
+% Inputs:
+%   sceneGeometryFileName - Full path to the sceneGeometry file. If left
+%                           empty, then an empty variable will be returned.
+%   verbosity             - Character vector. A valid value is "full".
+%
+% Outputs:
+%   sceneGeometry         - The sceneGeometry structure
+%   
+
+if isempty(sceneGeometryFileName)
+    sceneGeometry=[];
+else
+    % load the sceneGeometry structure
+    dataLoad=load(sceneGeometryFileName);
+    sceneGeometry=dataLoad.sceneGeometry;
+    clear dataLoad
+    % instantiate the ray-tracing function
+    if ~isempty(sceneGeometry.virtualImageFunc)
+        % The field is not empty, so we should have a ray tracing function.
+        try
+            funResult = functions(sceneGeometry.virtualImageFunc.handle);
+        catch
+            error('Invalid function definition in sceneGeometry.virtualImageFunc');
+        end
+        switch funResult.type
+            case 'anonymous'
+                % No further action needed
+                if strcmp(verbosity,'full')
+                    fprintf('Anonymous ray trace function available\n');
+                end
+            case 'simple'
+                % Determine if the function exists
+                if exist(func2str(sceneGeometry.virtualImageFunc.handle))==0
+                    % We need to add the function back to the path
+                    addpath(fileparts(sceneGeometry.virtualImageFunc.path))
+                    % Check to make sure that it is now available
+                    if exist(func2str(sceneGeometry.virtualImageFunc.handle))==0
+                        error('Unable to re-instantiate the ray tracing function')
+                    end
+                end
+                if strcmp(verbosity,'full')
+                    fprintf('Compiled ray trace function available\n');
+                end
+            otherwise
+                error('Unrecognized function type');
+        end
+    end
+end
+
+end
