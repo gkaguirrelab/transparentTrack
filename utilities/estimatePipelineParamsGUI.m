3633b41f703c1c1800862d287e74fd628db0e01d
gkaguirre <gkaguirre@me.com>
2018-06-16 13:30:50 -0400

    make GetWithDefault a local function
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- utilities/estimatePipelineParamsGUI.m
+++ utilities/estimatePipelineParamsGUI.m
@@ -1,4 +1,4 @@
-function [ initialParams ] = transparentTrackGUI(grayVideoName, protocol, varargin)
+function [ initialParams ] = estimatePipelineParamsGUI(grayVideoName, protocol, varargin)
 
 %% Input parser
 p = inputParser; p.KeepUnmatched = true;
@@ -525,4 +525,24 @@ for ii = framesToCheck
     delete('temp.mat')
 end
 
+end % MAIN
+
+
+%%% LOCAL FUNCTIONS
+
+function inputVal = GetWithDefault(prompt,defaultVal)
+% inputVal = GetWithDefault(prompt,defaultVal)
+%
+% Prompt for a number or string, with a default returned if user
+% hits return.
+%
+% 4/3/10  dhb  Wrote it.
+
+if (ischar(defaultVal))
+    inputVal = input(sprintf([prompt ' [%s]: '],defaultVal),'s');
+else
+    inputVal = input(sprintf([prompt ' [%g]: '],defaultVal));
+end
+if (isempty(inputVal))
+    inputVal = defaultVal;
 end
