837864e9c94cdb58c6329821b8a4178aa3f46de1
harrison-mcadams <harrison.mcadams@gmail.com>
2018-06-20 16:50:53 -0400

    added ability to manually tweak params
    if the preview of fitPupilPerimeter shows poor results, the operator can now manually tweak the parameters and try again

--- utilities/estimatePipelineParamsGUI.m
+++ utilities/estimatePipelineParamsGUI.m
@@ -17,7 +17,7 @@ function [ initialParams ] = estimatePipelineParamsGUI(grayVideoName, approach, 
 %   the first frame), 4) pupilCircleThresh (a value which is used to
 %   differentiate which intensities correspond to pupil vs. surrounding
 %   iris), and 5) maximumVisibleIrisDiameter (defines the diameter of the
-%   iris seen in the frame in which the eye is looking straight ahead). 
+%   iris seen in the frame in which the eye is looking straight ahead).
 %
 %   The process of this routine is as follows. First, the inputted video is
 %   opened (using the system's default video program) allowing the user to
@@ -73,7 +73,7 @@ function [ initialParams ] = estimatePipelineParamsGUI(grayVideoName, approach, 
 %                           position, ellipse area, eccentricity, and angle
 %                           of tilt.
 %  'pupilGammaCorrection' - A number. Gamma correction to be applied to the
-%                           video frames (default 1, typical values between 
+%                           video frames (default 1, typical values between
 %                           0.5 and 1.8)
 %  'frameMaskValue'       - A number. The image value that is assigned to
 %                           the region that is masked by frameMask. This
@@ -294,7 +294,7 @@ if p.Results.frameNumber ==1
         videoInObj.CurrentTime = (frameRequest - 1)/(videoInObj.FrameRate);
         thisFrame = readFrame(videoInObj);
         thisFrame = rgb2gray(thisFrame);
-        thisFrame = squeeze(thisFrame);        
+        thisFrame = squeeze(thisFrame);
         figure;
         imshow(thisFrame, 'Border', 'tight');
         hold on
@@ -598,7 +598,7 @@ if (min(potentialIndices)-1) < 1 || (min(potentialIndices)-1) > length(p.Results
     intensityDivider = x;
     close(plotFig);
     [minValue, potentialIndices] = min(abs(yy-intensityDivider));
-
+    
 end
 
 
@@ -722,6 +722,116 @@ for ii = framesToCheck
     imshow(displayFrame, 'Border', 'tight')
     dText = text(1,10,string, 'FontSize', 16, 'BackgroundColor', 'white');
     delete('temp.mat')
+end
+
+%% allow the user to adjust certain parameters, then test finding the pupil perimeter again
+adjustParamsChoice = GetWithDefault('>> Satisfied with these parameters? Enter ''y'' to proceed and exit, or ''n'' to manually adjust the parameters. [y/n]', 'y');
+if ~strcmp(adjustParamsChoice, 'y')
+    close all
+    adjustParamsFlag = false;
+    while ~adjustParamsFlag
+        
+        fprintf('Select the parameter you would like to adjust:\n')
+        fprintf('\t1. ellipseTransparentUB: %g %g %g %g %g \n', ellipseTransparentUB(:));
+        fprintf('\t2. ellipseTransparentLB: %g %g %g %g %g \n', ellipseTransparentLB(:));
+        fprintf('\t3. pupilGammaCorrection: %g\n', pupilGammaCorrection);
+        fprintf('\t4. frameMaskValue: %g\n', frameMaskValue);
+        fprintf('\t5. pupilFrameMask: %g %g %g %g\n', initialParams.pupilFrameMask(:));
+        fprintf('\t6. pupilCircleThresh: %g\n', initialParams.pupilCircleThresh);
+        fprintf('\t7. maskBox: %g %g\n', maskBox(:));
+        fprintf('\t8. pupilRange: %g %g\n', initialParams.pupilRange(:));
+
+        choice = input('\nYour choice: ', 's');
+        
+        switch choice
+            case '1'
+                ellipseTransparentUB = input('Enter new ellipseTransparentUB:     ');
+            case '2'
+                ellipseTransparentLB = input('Enter new ellipseTransparentLB:     ');
+            case '3'
+                pupilGammaCorrection = input('Enter new pupilGammaCorrection:     ');
+            case '4'
+                frameMaskValue = input('Enter new frameMaskValue:     ');
+            case '5'
+                initialParams.pupilFrameMask = input('Enter new pupilFrameMask:     ');
+            case '6'
+                initialParams.pupilCircleThresh = input('Enter new pupilCircleThresh:     ');
+            case '7'
+                maskBox = input('Enter new maskBox:     ');
+            case '8'
+                initialParams.pupilRange = input('Enter new pupilRange:     ');
+        end
+        
+        fprintf('New parameters:\n')
+        fprintf('\tellipseTransparentUB: %g %g %g %g %g \n', ellipseTransparentUB(:));
+        fprintf('\tellipseTransparentLB: %g %g %g %g %g \n', ellipseTransparentLB(:));
+        fprintf('\tpupilGammaCorrection: %g\n', pupilGammaCorrection);
+        fprintf('\tframeMaskValue: %g\n', frameMaskValue);
+        fprintf('\tpupilFrameMask: %g %g %g %g\n', initialParams.pupilFrameMask(:));
+        fprintf('\tpupilCircleThresh: %g\n', initialParams.pupilCircleThresh);
+        fprintf('\tmaskBox: %g %g\n', maskBox(:));
+        fprintf('\tpupilRange: %g %g\n', initialParams.pupilRange(:));
+
+        
+        nFramesToCheck = 4;
+        for ii = 1:nFramesToCheck
+            framesToCheck(ii) = round((nFrames/(nFramesToCheck-1))*(ii-1))+1;
+        end
+        framesToCheck(1) = p.Results.frameNumber;
+        framesToCheck(end) = nFrames;
+        
+        
+        counter = 1;
+        for ii = framesToCheck
+            perimeter = [];
+            plotFig = figure;
+            hold on
+            
+            %subplot(2, round(nFrames/2), counter)
+            counter = counter + 1;
+            string = [];
+            string = (['Frame ', num2str(ii)]);
+            
+            videoInObj.CurrentTime = (ii - 1)/(videoInObj.FrameRate);
+            thisFrameDiagnostics = readFrame(videoInObj);
+            thisFrameDiagnostics = rgb2gray(thisFrameDiagnostics);
+            thisFrameDiagnostics = squeeze(thisFrameDiagnostics);
+            
+            
+            perimeter = findPupilPerimeter(grayVideoName, 'temp', ...
+                'startFrame', ii, ...
+                'nFrames', 1, ...
+                'ellipseTransparentUB', ellipseTransparentUB, ...
+                'ellipseTransparentLB', ellipseTransparentLB, ...
+                'pupilGammaCorrection', pupilGammaCorrection, ...
+                'frameMaskValue', frameMaskValue, ...
+                'pupilFrameMask', initialParams.pupilFrameMask, ...
+                'pupilRange', initialParams.pupilRange, ...
+                'pupilCircleThresh', initialParams.pupilCircleThresh, ...
+                'maskBox', maskBox);
+            displayFrame=thisFrameDiagnostics;
+            if ~isempty(perimeter.data{1}.Xp)
+                displayFrame(sub2ind(size(thisFrameDiagnostics),perimeter.data{1}.Yp,perimeter.data{1}.Xp))=255;
+            end
+            if isempty(perimeter.data{1}.Xp)
+                string = 'No pupil found';
+                text(350, 200, string);
+            end
+            imshow(displayFrame, 'Border', 'tight')
+            dText = text(1,10,string, 'FontSize', 16, 'BackgroundColor', 'white');
+            delete('temp.mat')
+        end
+        adjustParamsChoice = GetWithDefault('>> Satisfied with these parameters? Enter ''y'' to proceed and exit, or ''n'' to manually adjust the parameters. [y/n]', 'y');
+        switch adjustParamsChoice
+            case 'y'
+                adjustParamsFlag = true;
+            case 'n'
+                adjustParamsFlag = false;
+                close all
+        end
+        
+        
+    end
 end
 
 end % MAIN
