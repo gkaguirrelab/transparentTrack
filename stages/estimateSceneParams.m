23305b6918ece4d96ab7bd1fef556d55eb970f24
gkaguirre <gkaguirre@me.com>
2018-06-22 13:27:33 -0400

    Add to the plot a list of the frames that provided ellipses
    This will support looking at these and editing the list
    
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- stages/estimateSceneParams.m
+++ stages/estimateSceneParams.m
@@ -258,6 +258,7 @@ if p.Results.nBADSsearches==0
     allSceneParamResults = [];
     sceneParamResultsMean = (p.Results.sceneParamsLBp+p.Results.sceneParamsUBp)./2;
     sceneParamResultsSD=[];
+    allFvals = [];
 else
     % Loop over the requested number of BADS searches
     searchResults = {};
@@ -663,6 +664,11 @@ legend({'0',num2str(sceneGeometry.constraintTolerance/2), ['=> ' num2str(sceneGe
 xFinal = sceneGeometry.meta.estimateSceneParams.search.x;
 myString = sprintf('torsion [deg] %4.1f; translation vector [mm] = %4.1f, %4.1f, %4.1f; rotation center scaling [joint, differential] = %4.2f, %4.2f',xFinal(1),xFinal(2),xFinal(3),xFinal(4),xFinal(5),xFinal(6));
 text(0.5,1.0,myString,'Units','normalized','HorizontalAlignment','center')
+
+% Add text to report the ellipse frames used
+ellipseFrameList = num2str(sort(sceneGeometry.meta.estimateSceneParams.search.ellipseArrayList)');
+myString = sprintf(['Ellipse frames: ' ellipseFrameList]);
+text(0.5,0.1,myString,'Units','normalized','HorizontalAlignment','center')
 
 %% Right panel -- area error
 subplot(3,3,[3 6]);
