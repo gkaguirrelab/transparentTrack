4e56b56d236426e730da6722d3cdf9bee36498d6
gkaguirre <gkaguirre@me.com>
2018-08-15 14:01:37 -0400

    Working GUI tool
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- utilities/estimateSceneParamsGUI.m
+++ utilities/estimateSceneParamsGUI.m
@@ -33,10 +33,6 @@ clear dataLoad
 
 % Identify the frames of the ellipse array
 ellipseArrayList = sceneGeometry.meta.estimateSceneParams.ellipseArrayList;
-ellipseArrayList = ellipseArrayList(1);
-
-% Set the initial scene params
-sceneVector = sceneGeometry.meta.estimateSceneParams.search.x;
 
 % Load the pupil perimeter data. It will be a structure variable
 % "perimeter", with the fields .data and .meta
@@ -58,76 +54,141 @@ nFrames = floor(videoInObj.Duration*videoInObj.FrameRate);
 % Define a variable to hold the selected frames
 sourceFrames = zeros(videoSizeY,videoSizeX,3,length(ellipseArrayList),'uint8');
 for ii = 1:length(ellipseArrayList)
-    idx = ellipseArrayList(ii);
+    frameIdx = ellipseArrayList(ii);
     % Obtain and render the frame
-    videoInObj.CurrentTime = (idx - 1)/(videoInObj.FrameRate);
+    videoInObj.CurrentTime = (frameIdx - 1)/(videoInObj.FrameRate);
     sourceFrames(:,:,:,ii) = readFrame(videoInObj);
 end
 
-    % close the video object
-    clear videoInObj
-
-% Define a variable to hold the selected frames
-framesToMontage = zeros(videoSizeY,videoSizeX,3,length(ellipseArrayList),'uint8');
-
-% Turn off a warning that can occur during the montage step
-warningState = warning;
-warning('off','images:initSize:adjustingMag');
+% close the video object
+clear videoInObj
 
 % Provide some instructions for the operator
-fprintf('Adjust camera translation with i-j-k-l.\n');
-fprintf('Adjust camera torsion with the left and right arrow keys.\n');
+fprintf('Adjust horizontal /vertical camera translation with the arrow keys.\n');
+fprintf('Adjust depth camera translation with + and -.\n');
+fprintf('Adjust camera torsion with j and k.\n');
+fprintf('Move forward and backward in the ellipse frames with a and s\n')
 fprintf('Press return to be prompted to enter scene param values as text.\n');
 fprintf('Press esc to exit.\n');
 
-% Prepare a figure
+% Set the current index and scene params
+arrayIdx = 1;
+x = sceneGeometry.meta.estimateSceneParams.search.x;
+
+% Prepare the main figure
+figHandle=figure('Visible','on');
+annotHandle=[];
 
 % Enter a while loop
 notDoneFlag = true;
 
 while notDoneFlag
-    for ii = 1:length(ellipseArrayList)
-tic
-        idx = ellipseArrayList(ii);
-        
-        % Obtain the eye pose from the boundary points from the perimeter
-        Xp = perimeter.data{idx}.Xp;
-        Yp = perimeter.data{idx}.Yp;
-        eyePose = eyePoseEllipseFit(Xp, Yp, sceneGeometry, 'repeatSearchThresh', 10);
-        
-        % Add the rendered eye model
-        if ~any(isnan(eyePose))
-            sourceFrame = squeeze(sourceFrames(:,:,:,ii));
-            [~,~,thisFrame] = renderEyePose(eyePose, sceneGeometry, ...
-                'newFigure', true, 'visible', false, ...
-                'backgroundImage',sourceFrame, ...
-                'modelEyeLabelNames', {'retina' 'irisPerimeter' 'pupilEllipse' 'cornea'}, ...
-                'modelEyePlotColors', {'.w' '.b' '-g' '.y'}, ...
-                'modelEyeSymbolSizeScaler',1.5,...
-                'modelEyeAlpha', 0.25);
-        end
-        % Add a text label for the frame number
-        frameLabel = sprintf('frame: %d',idx);
-        thisFrame.cdata = insertText(thisFrame.cdata,[20 20],frameLabel,'FontSize',30);
-        % Store the frame
-        framesToMontage(:,:,:,ii) = thisFrame.cdata;
+    
+    % Update the scene parameters
+    candidateSceneGeometry = sceneGeometry;
+    candidateSceneGeometry.cameraPosition.torsion = x(1);
+    candidateSceneGeometry.cameraPosition.translation = x(2:4);
+    
+    % Identify the frame to display
+    frameIdx = ellipseArrayList(arrayIdx);
+    
+    % Obtain the eye pose from the boundary points from the perimeter
+    Xp = perimeter.data{frameIdx}.Xp;
+    Yp = perimeter.data{frameIdx}.Yp;
+    eyePose = eyePoseEllipseFit(Xp, Yp, candidateSceneGeometry, 'repeatSearchThresh', 10);
+
+    % Show this video frame
+    thisFrame = sourceFrames(:,:,:,arrayIdx);
+    frameLabel = sprintf('frame: %d',frameIdx);
+    thisFrame = insertText(thisFrame,[20 20],frameLabel,'FontSize',30);    
+    imshow(squeeze(thisFrame));
+
+    % Add the rendered eye model
+    if ~any(isnan(eyePose))
+        renderEyePose(eyePose, candidateSceneGeometry, ...
+            'newFigure', false, 'visible', true, ...
+            'showAzimuthPlane', true, ...
+            'modelEyeLabelNames', {'retina' 'irisPerimeter' 'pupilEllipse' 'cornea'}, ...
+            'modelEyePlotColors', {'.w' '.b' '-g' '.y'}, ...
+            'modelEyeSymbolSizeScaler',1.5,...
+            'modelEyeAlpha', 0.25);
     end
 
-    
-    % Prepare the main figure
-    figHandle=figure('Visible','on');
-
-    % Create the montage
-    if length(ellipseArrayList)==1
-        imshow(squeeze(framesToMontage(:,:,:,1)));
-    else
-        montage(framesToMontage);
+    if ~isempty(annotHandle)
+        delete(annotHandle)
     end
-    drawnow
-    toc
-    pause
     
-    close figHandle
+    % Wait for operator input
+    fprintf('torsion: %0.2f, translation [%0.2f; %0.2f; %0.2f]\n',x(1),x(2),x(3),x(4));
+    waitforbuttonpress
+    keyChoiceValue = double(get(gcf,'CurrentCharacter'));
+    switch keyChoiceValue
+        case 28
+            text_str = 'translate left';
+            x(2)=x(2)-0.5;
+        case 29
+            text_str = 'translate right';
+            x(2)=x(2)+0.5;
+        case 30
+            text_str = 'translate up';
+            x(3)=x(3)+0.5;
+        case 31
+            text_str = 'translate down';
+            x(3)=x(3)-0.5;
+        case {45 95}
+            text_str = 'translate closer in';
+            x(4)=x(4)-1;
+        case {61 43}
+            text_str = 'translate farther away';
+            x(4)=x(4)+1;
+        case 97
+            text_str = 'prior frame';
+            arrayIdx = arrayIdx-1;
+            if arrayIdx == 0
+                arrayIdx = length(ellipseArrayList);
+            end
+        case 115
+            text_str = 'next frame';
+            arrayIdx = arrayIdx+1;
+            if arrayIdx > length(ellipseArrayList)
+                arrayIdx = 1;
+            end
+        case 106
+            text_str = 'counter-clockwise torsion';
+            x(1)=x(1)-1;
+        case 107
+            text_str = 'clockwise torsion';
+            x(1)=x(1)+1;
+        case 13
+            x = input('Enter values in square brackets, separated by semi-colons [tor;x;y;z]:');
+            text_str = 'manual param entry';
+        case 27
+            notDoneFlag = false;
+        otherwise
+            text_str = 'unrecognized command';
+    end
+    
+    if notDoneFlag
+        annotHandle = annotation('textbox',...
+            [.80 .85 .1 .1],...
+            'HorizontalAlignment','center',...
+            'VerticalAlignment','middle',...
+            'Margin',1,...
+            'String',text_str,...
+            'FontSize',9,...
+            'FontName','Helvetica',...
+            'EdgeColor',[1 1 1],...
+            'LineWidth',1,...
+            'BackgroundColor',[0.9  0.9 0.9],...
+            'Color',[1 0 0]);
+        drawnow
+        hold off
+    end
 end
+
+% Clean up
+close(figHandle)
+fprintf('\n');
+fprintf('scene parameters = [%0.2f; %0.2f; %0.2f; %0.2f; %0.2f; %0.2f]\n',x(1),x(2),x(3),x(4),x(5),x(6));
 
 end % GetWithDefault
