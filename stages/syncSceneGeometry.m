5a6a9e04d7c2ad55440d4426ee4f052b45ab06b1
gkaguirre <gkaguirre@me.com>
2019-12-01 20:52:38 -0500

    Improving the weighting of adjusted eyePose
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- stages/syncSceneGeometry.m
+++ stages/syncSceneGeometry.m
@@ -572,9 +572,9 @@ for ii = 1:runLength
         sceneGeometryAdjusted,'x0',eyePoseFixed);
 end
 % Take the weighted median across eyePoses
-rmseVals = pupilData.initial.ellipses.RMSE(startIndex:startIndex+runLength);
+weights = weightFunc(pupilData,perimeter,startIndex,runLength);
 for ii=1:4
-    eyePoseAdjusted(ii) = medianw(eyePoseByFrame(:,ii),rmseVals);
+    eyePoseAdjusted(ii) = medianw(eyePoseByFrame(:,ii),weights);
 end
 
 sceneGeometryAdjusted.screenPosition.fixationAngles = -eyePoseAdjusted(1:3);
@@ -854,4 +854,56 @@ padded = padarray(image, [padY padX],125);
 rot = imrotate(padded, angle, method, 'crop');
 % Crop the image.
 output = rot(padY+1-shiftY:end-padY-shiftY, padX+1-shiftX:end-padX-shiftX, :);
+end
+
+
+function weights = weightFunc(pupilData,perimeter,startIndex,runLength)
+%% Calculate likelhood SD across frames
+% Obtain a measure for each frame of how completely the perimeter points
+% define a full, 360 degrees around the pupil. This index of coverage of
+% the pupil perimeter is distVals. If there is a perfectly uniform angular
+% distribution of points in space around the pupil perimeter, then the
+% distVals value will be zero. If there are perimeter points only at a
+% single angular location around the pupil cirle, then the distVal will 1.
+
+% The likelihood SD is based upon the RMSE of the fit of the elipse to the
+% perimeter points for each frame
+RMSE = pupilData.initial.ellipses.RMSE(startIndex:startIndex+runLength-1)';
+
+% Define the bins over which the distribution of perimeter angles will be
+% evaluated. 20 bins works pretty well.
+nDivisions = 20;
+histBins = linspace(-pi,pi,nDivisions);
+
+% Anonymous function returns the linear non-uniformity of a set of values,
+% ranging from 0 when perfectly uniform to 1 when completely non-uniform.
+nonUniformity = @(x) (sum(abs(x/sum(x)-mean(x/sum(x))))/2)/(1-1/length(x));
+
+% Loop over frames. Frames which have no perimeter points will be given a
+% distVal of NaN.
+for ii = 1:runLength
+    
+    % Obtain the center of this fitted ellipse
+    centerX = pupilData.(p.Results.fitLabel).ellipses.values(startIndex+ii-1,1);
+    centerY = pupilData.(p.Results.fitLabel).ellipses.values(startIndex+ii-1,2);
+    
+    % Obtain the set of perimeter points
+    Xp = perimeter.data{startIndex+ii-1}.Xp;
+    Yp = perimeter.data{startIndex+ii-1}.Xp;
+    
+    % Calculate the deviation of the distribution of points from uniform
+    linearNonUniformity(ii) = nonUniformity(histcounts(atan2(Yp-centerY,Xp-centerX),histBins));
+end
+
+% Subject the linearNonUniformity vector to a non-linear transformation.
+% This has the effect of changing 0 --> 0.1, 0.8 --> 1, and values > 0.8
+% --> infinity. This causes pupil perimeters with support at a single
+% location (as opposed to fully around the perimeter) to have a markedly
+% increased likelihood SD. Also, set InF values to something arbitrarily
+% large.
+distVals = (1./(1-sqrt(linearNonUniformity)))./10;
+distVals(isinf(distVals)) = 1e20;
+
+% The weight for each frame is the inverse of the RMSE multiplied by the distVal
+weights = 1./(distVals.*RMSE);
 end
