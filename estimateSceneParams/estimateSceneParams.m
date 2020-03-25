b922a8d1c934767dcb543904c329dd1e47b8d03c
gkaguirre <gkaguirre@me.com>
2020-03-25 11:24:11 -0400

    Lot’s of comments
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- estimateSceneParams/estimateSceneParams.m
+++ estimateSceneParams/estimateSceneParams.m
@@ -1,5 +1,5 @@
 function estimateSceneParams(videoStemName, frameSet, gazeTargets, varargin)
-% Estimate scene geometry parameters from pupil and glint measurements
+% Estimate eye and scene geometry parameters for one or more scenes
 %
 % Syntax:
 %  sceneGeometry = estimateSceneParams(pupilFileName, perimeterFileName, glintFileName, sceneGeometryFileName, varargin)
@@ -8,28 +8,54 @@ function estimateSceneParams(videoStemName, frameSet, gazeTargets, varargin)
 %   The appearance of the eye in a camera image is influenced by several
 %   parameters, including biometric properties of the eye and the position
 %   of the camera. This function uses a set of observations of the pupil
-%   and the glint to estimate these scene parameters. The accuracy of the
-%   estimate is much improved by supplying the location of visual targets
-%   that the eye was fixated upon during each of the observations. It is
-%   also valuable to provide a reasonably accurate value for the distance
-%   of the camera from the eye in the sceneParamsX0 vector.
+%   and the glint to estimate these scene parameters, including the location of visual targets
+%   that the eye was fixated upon during each of the observations. A set of
+%   measurements made with the camera in a fixed location constitutes a
+%   "scene". The routine may be supplied with information from more than
+%   one scene. The routine forces the eye biometric parameters to be the
+%   same in all scenes, but allows the scene parameters (camera position)
+%   to vary between scenes.
+%
+%   The accuracy of the solution is greatly improved by supplying an
+%   accurate initial value for the distance of the camera from the eye in
+%   the sceneParamsX0 vector. Unless the scenes feature the camera in very
+%   different positions, it is difficult for the solution to distinguish
+%   between the depth position of the camera and properties of eye biometry
+%   (rotation depth and corneal curvature). The routine includes a
+%   regularization that penalizes the solution for departing from the x0
+%   value for camera depth. The penalty rises as the square of the distance
+%   in mm of the camera depth from the inital value, with the weight of the
+%   penalty adjusted using the key 'depthChangePenaltyWeight'. Setting this
+%   weight to zero will remove the penalty.
 %
 % Inputs:
-%	pupilFileName
-%   perimeterFileName
-%   glintFileName         - Full path to these files.
-%   sceneGeometryFileName - Full path to the file in which the
-%                           sceneGeometry data should be saved
+%	videoStemName         - Cell array of length n of char vectors. Full
+%                           path to the n video files from which the scene
+%                           observations have been derived. The stem name
+%                           should omit the "_gray.avi" suffix that is
+%                           usually present in the names of these video
+%                           files. The routine assumes that files to be
+%                           used in the analysis have the suffixes:
+%                               {'_correctedPerimeter.mat','_glint.mat', ...
+%                               '_pupil.mat'}
+%                           The sceneGeometry output files will be saved at
+%                           the same path location with the suffix:
+%                               '_sceneGeometry.mat'
+%   frameSet              - Cell array of length n of numeric vectors. Each
+%                           vector specifies the m frame indices (indexed
+%                           from 1) which identify the set of frames from
+%                           the scene to guide the search.
+%   gazeTargets           - Cell array of length n of  2xm matrices that
+%                           provide the positions, in degrees of visual
+%                           angle, of fixation targets that correspond to
+%                           each of the frames. The visual angle of the
+%                           stimuli should be adjusted for
+%                           min/magnification produced by spectacle lenses
+%                           worn by the subject prior to being passed to
+%                           this routine.
 %
 % Optional key/value pairs (display and I/O):
 %  'verbose'              - Logical. Default false.
-%  'pupilFileToVideoSuffixSwitch' - Cell array that provides the suffix
-%                           of the pupilData file and the suffix of the
-%                           corresponding fit video file. This way, the fit
-%                           video corresponding to the passed pupilData
-%                           file can be found and used to create the
-%                           ellipse array montage plot.
-%  'saveDiagnosticPlot'   - Logical.
 %
 % Optional key/value pairs (flow control)
 %  'useParallel'          - If set to true, use the MATLAB parallel pool
@@ -47,62 +73,100 @@ function estimateSceneParams(videoStemName, frameSet, gazeTargets, varargin)
 %  'hostname'             - AUTOMATIC; The host
 %
 % Optional key/value pairs (analysis)
+%  'eyeArgs'              - Cell array. This is a cell array of key-value
+%                           pairs to be used when generating the eye model
+%                           for this subject. An example input is:
+%                           {'axialLength',23.45,'sphericalAmetropia',-0.5}
+%                           Note that these key values:
+%                               {'kvals','rotationCenterScalers',...
+%                               'primaryPosition'}
+%                           should not be passed, as they will be
+%                           supplanted parameters in the search. If you
+%                           wish to provide and lock these parameters in
+%                           the search, do so by providing the values in
+%                           the x0 arrays, and setting the corresponding
+%                           values in the param bounds to zero.
+%  'sceneArgs'            - Cell array of length n of cell arrays. Each
+%                           array contians key-value pairs to be used when
+%                           generating the scene model for a particular
+%                           video. An example input (for 2 scenes ) is:
+%                           { {'spectacleLens',-3} , {'spectacleLens',-3} }
+%  'useFixForPrimaryPos'  - Logical. The primary position of the eye is
+%                           specified as part of the eye model, and
+%                           controls the pseudo-torsion of the eye
+%                           following Listing's Law. Because eye rotations
+%                           are defined with respect to the alignment of
+%                           the optical axis of the camera with the optical
+%                           axis of the eye, the primary position of the
+%                           eye in the scene coordinate frame will vary
+%                           with camera position. The routine is able to
+%                           search across primary position values for each
+%                           scene. This flag causes the model to be updated
+%                           after each stage such that the primary position
+%                           for the eye in a given scene is set equal to
+%                           the modeled position of the eye when it is
+%                           fixating position [0 0] of a fixation array.
+%                           The use of this flag is justified when the
+%                           subject is allowed to adjust their head to most
+%                           comfortably view the center of a fixation array
+%                           prior to recording, presumably placing the
+%                           center of fixation at their primary position.
+%  'eyeParamsX0'          - 1x7 vector. Provides the initial values for the
+%                           parameters that control the biometric
+%                           properties of the eye The order of these
+%                           parameters is specified in the hard-coded
+%                           variable: eyeParamLabels.
+%  'eyeParamsBounds'      - 1x7 vector. These values are added to and
+%                           subtracted from the x0 vector to define the
+%                           hard limits of the search.
+%  'sceneParamsX0'        - Cell array of length n of 1x6 vectors. Provides
+%                           the initial values for the paramters that
+%                           control camera position in each scene. It is
+%                           reasonable to set all of these parameter values
+%                           to zero, with the exception of the depth
+%                           parameter, which should be as accurate as
+%                           possible. The order of the parameters is
+%                           specified in the variable: sceneParamLabels
+%  'sceneParamsBounds'    - 1x6 vector. These values are added to and
+%                           subtracted from the x0 vector to define the
+%                           hard limits of the search. The values in this
+%                           vector are used for all scenes.
 %  'eyePoseLB/UB'         - 1x4 vector. Upper / lower bounds on the eyePose
 %                           [azimuth, elevation, torsion, pupil radius].
-%                           The torsion value is unusued and is bounded to
-%                           zero. Biological limits in eye rotation and
-%                           pupil size would suggest boundaries of [±35,
-%                           ±25, 0, 0.25-4]. Note, however, that these
-%                           angles are relative to the center of
-%                           projection, not the primary position of the
-%                           eye. Therefore, in circumstances in which the
-%                           camera is viewing the eye from an off-center
-%                           angle, the bounds will need to be shifted
-%                           accordingly.
-%  'frameSet'             - A vector of m frame numbers (indexed from 1)
-%                           which identify the ellipses to be used for the
-%                           estimation of scene geometry. If left empty,
-%                           a list of ellipses will be generated.
-%  'gazeTargets'          - A 2xm matrix that provides the positions, in
-%                           nominal degrees of visual angle, of fixation
-%                           targets that correspond to each of the frames
-%                           identified in the ellipseArrayList. If defined,
-%                           the routine will find the scene geometry that
-%                           best aligns the recovered eye poses with the
-%                           fixation targets, subject to a translation and
-%                           rotation matrix. If left empty, the search will
-%                           minimize error in the joint specification of
-%                           the glint and pupil perimeter. If needed, the
-%                           visual angle of the stimuli will be adjusted
-%                           for min/magnification produced by spectacle
-%                           lenses worn by the subject.
-%  'fixSpectacleLens'     - Scalar. This parameter is used to handle the
-%                           unusual circumstance in which the eye viewing a
-%                           fixation array is behind a spectacle lens, but
-%                           the eye being modeled for pupil appearance is
-%                           not. Setting this parameter causes the routine
-%                           to calculate a magnification factor for the
-%                           fixation target array, but does not apply this
-%                           spectacle to the eye being modeled.
-%  'searchThresh'         - Scalar. Search iterations will be repeated
-%                           until the fVal is below this threshold, or the
-%                           maximum number of search iterations has been
-%                           reached.
-%  'searchIterations'     - Scalar integer. The maximum number of search
-%                           iterations to conduct.
-%  'sceneParamsX0'        - 1x11 vector. Scene parameters to use as the
-%                           starting point for the search.
-%  'sceneParamsToLock'    - 1x11 vector. Any param positions set to unity
-%                           will be locked at their x0 values.
+%                           Biological limits in eye rotation and pupil
+%                           size would suggest boundaries of [±35, ±25, 0,
+%                           0.25-4]. Note, however, that these angles are
+%                           relative to the center of projection, not the
+%                           primary position of the eye. Therefore, in
+%                           circumstances in which the camera is viewing
+%                           the eye from an off-center angle, the bounds
+%                           will need to be shifted accordingly.
+%  'errorReg'             - 1x4 vector. Passed to calcGlintGazeError and
+%                           determines the relative weighting of the
+%                           different types of error in scene prediction.
+%  'multiSceneNorm'       - Scalar. Defines the metric used to combine
+%                           errors across scenes. Defaults to a value of 1
+%                           and thus the L1 norm.
+%  'TolMesh'              - Scalar. The precision with which the parameters
+%                           are adjusted within the BADS search. A value of
+%                           1e-2 provides a good trade-off between search
+%                           time and useful precision in the eye and scene
+%                           parameters.
+%  'depthChangePenaltyWeight' - Scalar. Adjustment of camera depth away
+%                           from the x0 values is penalized. The effect of
+%                           the weight (w) is such that an x% change in
+%                           camera depth produces a [(w*x)^2]% increase in
+%                           the objective. A weight value of zero removes
+%                           the regularization.
 %
 % Outputs
-%	sceneGeometry         - A structure that contains the components of the
-%                           projection model.
+%	None. Although sceneGeometry files and diagnostic plots are saved for
+%	each scene at the location specified by the videoStemName paths.
 %
 % Examples:
 %{
     % Get the DropBox base directory
-    dropboxBaseDir = getpref('eyeTrackTOMEAnalysis','dropboxBaseDir'); 
+    dropboxBaseDir = getpref('eyeTrackTOMEAnalysis','dropboxBaseDir');
     videoStemName = {fullfile(dropboxBaseDir,'TOME_processing/session2_spatialStimuli/TOME_3015/032417/EyeTracking/GazeCal01'), ...
                     fullfile(dropboxBaseDir,'TOME_processing/session2_spatialStimuli/TOME_3015/032417/EyeTracking/GazeCal02'), ...
                     fullfile(dropboxBaseDir,'TOME_processing/session2_spatialStimuli/TOME_3015/032417/EyeTracking/GazeCal03'), ...
@@ -148,7 +212,6 @@ p.addRequired('gazeTargets',@iscell);
 
 % Optional display and I/O params
 p.addParameter('verbose',true,@islogical);
-p.addParameter('saveDiagnosticPlot',true,@islogical);
 
 % Optional flow control params
 p.addParameter('useParallel',true,@islogical);
@@ -163,7 +226,6 @@ p.addParameter('hostname',char(java.net.InetAddress.getLocalHost.getHostName),@i
 % Optional analysis params
 p.addParameter('eyeArgs',{''},@iscell);
 p.addParameter('sceneArgs',{''},@iscell);
-p.addParameter('searchThresh',1.0,@isscalar);
 p.addParameter('useFixForPrimaryPos',true,@islogical);
 p.addParameter('eyeParamsX0',[44.2410, 45.6302, 0, 2.5000, 0, 1, 1],@isnumeric);
 p.addParameter('eyeParamsBounds',[5, 5, 90, 5, 5, 0.25, 0.15],@isnumeric);
@@ -213,10 +275,7 @@ blankSearch = uint8(zeros(1,nTotalParams));
 sceneIdxRep = @(idx) nEyeParams + repmat((0:nScenes-1)*nSceneParams,1,length(idx)) + ...
     cell2mat(arrayfun(@(x) repmat(x,1,nScenes),idx,'UniformOutput',false));
 
-% The search vectors 
-
-%% REPLACE THE HARD-CODED IDX WITH STRCMP WITH THE PARAM LABELS
-
+% The search vectors
 corneaSet = blankSearch; corneaSet(1:5)=1;
 rotationSet = blankSearch; rotationSet(6:7)=1;
 primaryPosSet = blankSearch; primaryPosSet(sceneIdxRep(1:2)) = 1;
@@ -258,7 +317,7 @@ for ss = 1:nScenes
         videoStemName{ss}, frameSet{ss}, gazeTargets{ss}, ...
         setupArgs, keyVals, p.Results, ...
         'verbose', p.Results.verbose);
-        
+    
     % Assemble the x0 by concatenating the x0 scene params
     x0 = [x0, p.Results.sceneParamsX0{ss}];
     
@@ -309,6 +368,12 @@ end
 %% Search across stages
 for ii = 1:nStages
     
+    % Announce
+    if p.Results.verbose
+        str = sprintf(['Starting stage %d of %d, ' searchSetLabels{ii} '\n',ii,nStages]);
+        fprintf(str);
+    end
+    
     % Bounds
     [x,lb,ub,lbp,ubp] = setBounds(x,xBounds,searchSets{ii});
     
@@ -352,44 +417,8 @@ if p.Results.verbose
     fprintf([num2str(executionTime) '\n']);
 end
 
-end
-
-
-
-
-
-
-%%%%%%%%%%%% LOCAL FUNCTIONS
-
-
-
-function [x,lb,ub,lbp,ubp] = setBounds(x,bb,searchSet)
-
-% Apply the bounds
-lb = x - bb;
-lbp = x - bb./2;
-ubp = x + bb./2;
-ub = x + bb;
-
-% Lock params
-lb(~searchSet) = x(~searchSet);
-ub(~searchSet) = x(~searchSet);
-lbp(~searchSet) = x(~searchSet);
-ubp(~searchSet) = x(~searchSet);
-
-% Keep the corneal axis (torsion) between +-90
-lb(3) = max([lb(3) -90]);
-ub(3) = min([ub(3) 90]);
-lbp(3) = max([lbp(3) -90]);
-ubp(3) = min([ubp(3) 90]);
-
-% Ensure that x is within bounds
-x=min([ub; x]);
-x=max([lb; x]);
 
 end
-
-
 
 
 
