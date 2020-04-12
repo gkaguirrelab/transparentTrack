49972c3e83bb756871755ecedb7025a228011cc2
gkaguirre <gkaguirre@me.com>
2020-04-11 21:05:12 -0400

    Save obj
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- estimateSceneParams/@sceneObj/saveSceneGeometry.m
+++ estimateSceneParams/@sceneObj/saveSceneGeometry.m
@@ -64,22 +64,21 @@ end
 sceneGeometry = createSceneGeometry(sceneGeometryVarargin{:});
 
 % Add the meta data
-sceneGeometry.meta.estimateSceneParams.p = obj.meta;
 sceneGeometry.meta.estimateSceneParams.x = obj.x;
 sceneGeometry.meta.estimateSceneParams.xHead = obj.x(model.func.fieldSetIdx('head','all'));
 sceneGeometry.meta.estimateSceneParams.xEye = obj.x(model.func.fieldSetIdx('eye','all'));
 sceneGeometry.meta.estimateSceneParams.xScene = obj.x(model.func.fieldSetIdx('scene','all'));
 sceneGeometry.meta.estimateSceneParams.fVal = obj.fVal;
-sceneGeometry.meta.estimateSceneParams.model = obj.model;
 sceneGeometry.meta.estimateSceneParams.sceneGeometryVarargin = sceneGeometryVarargin;
-sceneGeometry.meta.estimateSceneParams.modelEyePose = obj.modelEyePose;
-sceneGeometry.meta.estimateSceneParams.modelPupilEllipse = obj.modelPupilEllipse;
-sceneGeometry.meta.estimateSceneParams.modelGlintCoord = obj.modelGlintCoord;
-sceneGeometry.meta.estimateSceneParams.modelPoseGaze = obj.modelPoseGaze;
-sceneGeometry.meta.estimateSceneParams.modelVecGaze = obj.modelVecGaze;
-sceneGeometry.meta.estimateSceneParams.poseRegParams = obj.poseRegParams;
-sceneGeometry.meta.estimateSceneParams.vecRegParams = obj.vecRegParams;
-sceneGeometry.meta.estimateSceneParams.rawErrors = obj.rawErrors;
+sceneGeometry.meta.estimateSceneParams.obj = obj;
+% sceneGeometry.meta.estimateSceneParams.modelEyePose = obj.modelEyePose;
+% sceneGeometry.meta.estimateSceneParams.modelPupilEllipse = obj.modelPupilEllipse;
+% sceneGeometry.meta.estimateSceneParams.modelGlintCoord = obj.modelGlintCoord;
+% sceneGeometry.meta.estimateSceneParams.modelPoseGaze = obj.modelPoseGaze;
+% sceneGeometry.meta.estimateSceneParams.modelVecGaze = obj.modelVecGaze;
+% sceneGeometry.meta.estimateSceneParams.poseRegParams = obj.poseRegParams;
+% sceneGeometry.meta.estimateSceneParams.vecRegParams = obj.vecRegParams;
+% sceneGeometry.meta.estimateSceneParams.rawErrors = obj.rawErrors;
 
 % Save the sceneGeometry file
 sceneGeometryFileName = [obj.videoStemName '_sceneGeometry' fileNameSuffix '.mat'];
