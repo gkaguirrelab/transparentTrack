578c44ba038573e48c7f2d906986dbe6254d594a
gkaguirre <gkaguirre@me.com>
2020-07-11 09:35:04 -0400

    Remove debugging code
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- stages/smoothPupilRadius.m
+++ stages/smoothPupilRadius.m
@@ -296,8 +296,8 @@ end
 warnState = warning();
 
 % Loop through the frames
-%parfor (ii = 1:nFrames, nWorkers)
-for ii = 1:10 %nFrames
+parfor (ii = 1:nFrames, nWorkers)
+%for ii = 1:nFrames
     
     % update progress
     if verbose
