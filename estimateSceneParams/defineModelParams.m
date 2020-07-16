c31c17c1242341de5348abcc4b57406d8163eae9
gkaguirre <gkaguirre@me.com>
2020-07-16 11:50:56 -0400

    Allow for greater head movement in this strategy
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- estimateSceneParams/defineModelParams.m
+++ estimateSceneParams/defineModelParams.m
@@ -192,7 +192,7 @@ model.strategy.gazeCal.TolMesh = 1e-2;
 model.strategy.sceneSync.stages = { ...
     {'scene.cameraPosition', 'head.phaseAndRotation' } };
 model.strategy.sceneSync.errorArgs = { ...
-    {'cameraTransBounds',[1;1;0],'errorReg',[1 1 0 0 1]} };
+    {'cameraTransBounds',[5;5;0],'errorReg',[1 1 0 0 1]} };
 model.strategy.sceneSync.penaltyWeight = [100 1]; % [depth torsion]
 model.strategy.sceneSync.useFixForPrimaryPos = false;
 model.strategy.sceneSync.multiSceneNorm = 1;
