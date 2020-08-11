12d1b5723cc57c9dc9f5949894f86eb496552569
gkaguirre <gkaguirre@me.com>
2020-08-11 16:37:05 -0400

    Cleaning up this code
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- utilities/plotPupilDataEyePose.m
+++ utilities/plotPupilDataEyePose.m
@@ -32,8 +32,6 @@ function plotPupilDataEyePose( dataRootDir, plotSaveDir, varargin )
 %     - At the bottom of the plot, gray dots mark frames with high RMSE,
 %       and blue dots mark points where the fit hit the eyePose bounds.
 %
-%   Included local functions are subdir, by Kelly Kearney.
-%
 % Inputs:
 %   dataRootDir           - Full path to a directory that contains pupil
 %                           data files, and/or subdirectories containing
@@ -59,16 +57,13 @@ function plotPupilDataEyePose( dataRootDir, plotSaveDir, varargin )
 %
 % Examples:
 %{
-    % ETTBSkip -- This is an idiosyncratic example.
-    dataRootDir = '/Users/aguirre/Dropbox (Aguirre-Brainard Lab)/TOME_processing/session1_restAndStructure';
-    plotSaveDir = '/Users/aguirre/Dropbox (Aguirre-Brainard Lab)/TOME_processing/session1_restAndStructure/pupilDataQAPlots_eyePose'
-    plotPupilDataEyePose(dataRootDir,plotSaveDir);
-%}
-%{
     plotPupilDataEyePose( '', 'pupilDataQAPlots_eyePose_FLASH_July2020','acquisitionStem','tfMRI_FLASH','nColumns',2)
 %}
 %{
-    plotPupilDataEyePose( '', 'pupilDataQAPlots_eyePose_MOVIE_July2020','acquisitionStem','tfMRI_MOVIE','nColumns',4)
+    dropboxBaseDir=fullfile(getpref('eyeTrackTOMEAnalysis','dropboxBaseDir'));
+    dataRootDir=fullfile(dropboxBaseDir,'TOME_processing','session2_spatialStimuli');
+    dataSaveDir=fullfile(dataRootDir,'pupilDataQAPlots_eyePose_MOVIE_July2020');
+    plotPupilDataEyePose( dataRootDir, dataSaveDir,'acquisitionStem','tfMRI_MOVIE','nColumns',4)
 %}
 %{
     plotPupilDataEyePose( '', 'pupilDataQAPlots_eyePose_RETINO_July2020','acquisitionStem','tfMRI_RETINO','nColumns',4)
@@ -99,7 +94,7 @@ msecToMin = 1/1000/60;
 
 % Obtain the paths to all of the pupil data files within the specified
 % directory, including within sub-drectories.
-fileListStruct=subdir(fullfile(dataRootDir,[p.Results.acquisitionStem '*_pupil.mat']));
+fileListStruct=dir(fullfile(dataRootDir,'**',[p.Results.acquisitionStem '*_pupil.mat']));
 
 % If we found at least one pupil data file, then proceed.
 if ~isempty(fileListStruct)
@@ -173,9 +168,9 @@ if ~isempty(fileListStruct)
             ub_byParam = nan(1,length(p.Results.eyePoseParamsToPlot));
             for nn = 1:length(acqList)
                 % Obtain the path to this pupil data file and load it
-                pupilFullFileName = fullfile(fileListCell{1,acqList(nn)});
+                pupilFullFileName = fullfile(fileListCell{2,acqList(nn)},fileListCell{1,acqList(nn)});
                 load(pupilFullFileName,'pupilData');
-                glintFullFileName = strrep(fullfile(fileListCell{1,acqList(nn)}),'_pupil.mat','_glint.mat');
+                glintFullFileName = strrep(fullfile(fileListCell{2,acqList(nn)},fileListCell{1,acqList(nn)}),'_pupil.mat','_glint.mat');
                 load(glintFullFileName,'glintData');
                 
                 % Find the frames that lack a glint
@@ -203,7 +198,7 @@ if ~isempty(fileListStruct)
         for jj = 1: length(acqList)
             
             % Obtain the path to this pupil data file 
-            pupilFullFileName = fullfile(fileListCell{1,acqList(jj)});
+            pupilFullFileName = fullfile(fileListCell{2,acqList(jj)},fileListCell{1,acqList(jj)});
             load(pupilFullFileName,'pupilData');
 
             % Grab just the filename for this pupil data, omitting the
@@ -344,175 +339,3 @@ if ~isempty(fileListStruct)
 end % we have at least one session
 
 end % plotPupilDataEyePose
-
-
-
-%%% LOCAL FUNCTIONS
-
-
-function nameOut = escapeFileCharacters(nameIn)
-% Sanitize file strings to be used in system commands
-
-nameOut = strrep(nameIn,' ','\ ');
-nameOut = strrep(nameOut,'(','\(');
-nameOut = strrep(nameOut,')','\)');
-end
-
-
-
-
-
-function varargout = subdir(varargin)
-%SUBDIR Performs a recursive file search
-%
-% subdir
-% subdir(name)
-% files = subdir(...)
-%
-% This function performs a recursive file search.  The input and output
-% format is identical to the dir function.
-%
-% Input variables:
-%
-%   name:   pathname or filename for search, can be absolute or relative
-%           and wildcards (*) are allowed.  If ommitted, the files in the
-%           current working directory and its child folders are returned
-%
-% Output variables:
-%
-%   files:  m x 1 structure with the following fields:
-%           name:   full filename
-%           date:   modification date timestamp
-%           bytes:  number of bytes allocated to the file
-%           isdir:  1 if name is a directory; 0 if no
-%
-% Example:
-%
-%   >> a = subdir(fullfile(matlabroot, 'toolbox', 'matlab', '*.mat'))
-%
-%   a =
-%
-%   67x1 struct array with fields:
-%       name
-%       date
-%       bytes
-%       isdir
-%
-%   >> a(2)
-%
-%   ans =
-%
-%        name: '/Applications/MATLAB73/toolbox/matlab/audiovideo/chirp.mat'
-%        date: '14-Mar-2004 07:31:48'
-%       bytes: 25276
-%       isdir: 0
-%
-% See also:
-%
-%   dir
-
-% Copyright 2006 Kelly Kearney
-
-
-%---------------------------
-% Get folder and filter
-%---------------------------
-
-narginchk(0,1);
-nargoutchk(0,1);
-
-if nargin == 0
-    folder = pwd;
-    filter = '*';
-else
-    [folder, name, ext] = fileparts(varargin{1});
-    if isempty(folder)
-        folder = pwd;
-    end
-    if isempty(ext)
-        if isdir(fullfile(folder, name))
-            folder = fullfile(folder, name);
-            filter = '*';
-        else
-            filter = [name ext];
-        end
-    else
-        filter = [name ext];
-    end
-    if ~isdir(folder)
-        error('Folder (%s) not found', folder);
-    end
-end
-
-%---------------------------
-% Search all folders
-%---------------------------
-
-pathstr = genpath_local(folder);
-pathfolders = regexp(pathstr, pathsep, 'split');  % Same as strsplit without the error checking
-pathfolders = pathfolders(~cellfun('isempty', pathfolders));  % Remove any empty cells
-
-Files = [];
-pathandfilt = fullfile(pathfolders, filter);
-for ifolder = 1:length(pathandfilt)
-    NewFiles = dir(pathandfilt{ifolder});
-    if ~isempty(NewFiles)
-        fullnames = cellfun(@(a) fullfile(pathfolders{ifolder}, a), {NewFiles.name}, 'UniformOutput', false);
-        [NewFiles.name] = deal(fullnames{:});
-        Files = [Files; NewFiles];
-    end
-end
-
-%---------------------------
-% Prune . and ..
-%---------------------------
-
-if ~isempty(Files)
-    [~, ~, tail] = cellfun(@fileparts, {Files(:).name}, 'UniformOutput', false);
-    dottest = cellfun(@(x) isempty(regexp(x, '\.+(\w+$)', 'once')), tail);
-    Files(dottest & [Files(:).isdir]) = [];
-end
-
-%---------------------------
-% Output
-%---------------------------
-
-if nargout == 0
-    if ~isempty(Files)
-        fprintf('\n');
-        fprintf('%s\n', Files.name);
-        fprintf('\n');
-    end
-elseif nargout == 1
-    varargout{1} = Files;
-end
-
-end % subdir
-
-function [p] = genpath_local(d)
-% Modified genpath that doesn't ignore:
-%     - Folders named 'private'
-%     - MATLAB class folders (folder name starts with '@')
-%     - MATLAB package folders (folder name starts with '+')
-
-files = dir(d);
-if isempty(files)
-    return
-end
-p = '';  % Initialize output
-
-% Add d to the path even if it is empty.
-p = [p d pathsep];
-
-% Set logical vector for subdirectory entries in d
-isdir = logical(cat(1,files.isdir));
-dirs = files(isdir);  % Select only directory entries from the current listing
-
-for i=1:length(dirs)
-    dirname = dirs(i).name;
-    if    ~strcmp( dirname,'.') && ~strcmp( dirname,'..')
-        p = [p genpath(fullfile(d,dirname))];  % Recursive calling of this function.
-    end
-end
-
-end % genpath_local
